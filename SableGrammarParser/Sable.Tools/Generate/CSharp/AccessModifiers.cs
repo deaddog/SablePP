696eacb2bd5f08cd97d8b8b95f175ed96807cb2f
deaddog <mikkelslsl@gmail.com>
2013-06-05 00:07:55 +0200

    Emitting access modifiers for C# code is now done using an extension method on the AccessModifiers enum.

--- SableGrammarParser/Sable.Tools/Generate/CSharp/AccessModifiers.cs
+++ SableGrammarParser/Sable.Tools/Generate/CSharp/AccessModifiers.cs
@@ -1,4 +1,7 @@
-﻿namespace Sable.Tools.Generate.CSharp
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sable.Tools.Generate.CSharp
 {
     public enum AccessModifiers
     {
@@ -11,5 +14,51 @@
         @abstract = 64,
         @virtual = 128,
         @override = 256
+    }
+
+    internal static class AccessModifiersExtension
+    {
+        public static void Emit(this AccessModifiers modifiers, Action<string, UseSpace, UseSpace, object[]> emitter)
+        {
+            List<AccessModifiers> mods = new List<AccessModifiers>(SplitModifiers(modifiers));
+            mods.Sort(compare);
+            foreach (var a in mods)
+                emitter(a.ToString(), UseSpace.NotPreferred, UseSpace.Preferred, null);
+        }
+
+        private static IEnumerable<AccessModifiers> SplitModifiers(AccessModifiers modifiers)
+        {
+            foreach (AccessModifiers am in Enum.GetValues(typeof(AccessModifiers)))
+                if (modifiers.HasFlag(am))
+                    yield return am;
+        }
+
+        private static int compare(AccessModifiers mod1, AccessModifiers mod2)
+        {
+            return ToValue(mod1).CompareTo(ToValue(mod2));
+        }
+
+        private static int ToValue(AccessModifiers mod)
+        {
+            switch (mod)
+            {
+                case AccessModifiers.@public:
+                case AccessModifiers.@private:
+                case AccessModifiers.@protected:
+                    return 1;
+                case AccessModifiers.@partial:
+                    return 4;
+                case AccessModifiers.@new:
+                    return 0;
+                case AccessModifiers.@internal:
+                    return 2;
+                case AccessModifiers.@abstract:
+                case AccessModifiers.@virtual:
+                case AccessModifiers.@override:
+                    return 3;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
     }
 }
--- SableGrammarParser/Sable.Tools/Generate/CSharp/ClassElement.cs
+++ SableGrammarParser/Sable.Tools/Generate/CSharp/ClassElement.cs
@@ -15,7 +15,7 @@ namespace Sable.Tools.Generate.CSharp
             this.modifiers = modifiers;
             this.contents = new PatchElement();
 
-            EmitAccessModifiers();
+            modifiers.Emit(emit);
             emit("class", UseSpace.Always, UseSpace.Always);
             emit(name, UseSpace.Always, UseSpace.NotPreferred);
 
@@ -27,49 +27,6 @@ namespace Sable.Tools.Generate.CSharp
             decreaseIndentation();
             emit("}", UseSpace.Never, UseSpace.Never);
             emitNewLine();
-        }
-
-        private IEnumerable<AccessModifiers> SplitModifiers()
-        {
-            foreach (AccessModifiers am in Enum.GetValues(typeof(AccessModifiers)))
-                if (modifiers.HasFlag(am))
-                    yield return am;
-        }
-
-        private void EmitAccessModifiers()
-        {
-            List<AccessModifiers> mods = new List<AccessModifiers>(SplitModifiers());
-            mods.Sort(compare);
-            foreach (var a in mods)
-                emit(a.ToString(), UseSpace.NotPreferred, UseSpace.Preferred);
-        }
-
-        private int compare(AccessModifiers mod1, AccessModifiers mod2)
-        {
-            return ToValue(mod1).CompareTo(ToValue(mod2));
-        }
-
-        private int ToValue(AccessModifiers mod)
-        {
-            switch (mod)
-            {
-                case AccessModifiers.@public:
-                case AccessModifiers.@private:
-                case AccessModifiers.@protected:
-                    return 1;
-                case AccessModifiers.@partial:
-                    return 4;
-                case AccessModifiers.@new:
-                    return 0;
-                case AccessModifiers.@internal:
-                    return 2;
-                case AccessModifiers.@abstract:
-                case AccessModifiers.@virtual:
-                case AccessModifiers.@override:
-                    return 3;
-                default:
-                    throw new NotImplementedException();
-            }
         }
     }
 }
