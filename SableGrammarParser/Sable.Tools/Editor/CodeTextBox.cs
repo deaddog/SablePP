0da5f52bec8796b5a28e309020ff0dbf49cec698
deaddog <mikkelslsl@gmail.com>
2013-11-10 14:35:21 +0100

    Added the two textboxes that inherit the FastColoredTextBox.

---
+++ SableGrammarParser/Sable.Tools/Editor/CodeTextBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+using FastColoredTextBoxNS;
+
+namespace Sable.Tools.Editor
+{
+    public class CodeTextBox : FastColoredTextBox
+    {
+        public CodeTextBox()
+            : base()
+        {
+        }
+    }
+}
---
+++ SableGrammarParser/Sable.Tools/Editor/ErrorTextBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+using FastColoredTextBoxNS;
+
+namespace Sable.Tools.Editor
+{
+    public class ErrorTextBox : FastColoredTextBox
+    {
+        public ErrorTextBox()
+            : base()
+        {
+        }
+    }
+}
