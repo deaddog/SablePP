01ed2d3169e8f77247c529ee0e47ee22ee14077c
deaddog <mikkelslsl@gmail.com>
2013-11-11 19:46:43 +0100

    Added a builder for the DepthFirstAdapter.

---
+++ SableGrammarParser/Sable.Compiler/Generate/Analysis/DepthFirstAdapterBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Sable.Compiler.Nodes;
+using Sable.Tools.Generate.CSharp;
+
+namespace Sable.Compiler.Generate.Analysis
+{
+    public class DepthFirstAdapterBuilder : GenerateVisitor
+    {
+        private ClassElement adapterClass;
+
+        public DepthFirstAdapterBuilder(NameSpaceElement namespaceElement)
+        {
+            namespaceElement.CreateClass("DepthFirstAnalysisAdapter", AccessModifiers.@public, "DepthFirstAnalysisAdapter<object>");
+            adapterClass = namespaceElement.CreateClass("DepthFirstAnalysisAdapter", AccessModifiers.@public, "DepthFirstAnalysisAdapter<TValue, object>");
+            adapterClass.TypeParameters.Add("TValue");
+        }
+
+        public override void CaseAGrammar(AGrammar node)
+        {
+            if (node.HasAstproductions)
+                Visit(node.Astproductions);
+            else if (node.HasProductions)
+                Visit(node.Productions);
+
+            if (node.HasTokens)
+                Visit(node.Tokens);
+        }
+    }
+}
