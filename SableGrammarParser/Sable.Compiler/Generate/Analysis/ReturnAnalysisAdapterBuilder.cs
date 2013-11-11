99fe665787c1c0bc42ff16c1f6ace01e08cba21a
deaddog <mikkelslsl@gmail.com>
2013-11-11 21:05:54 +0100

    Added builder for ReturnAnalysisAdapter.

---
+++ SableGrammarParser/Sable.Compiler/Generate/Analysis/ReturnAnalysisAdapterBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Sable.Compiler.Nodes;
+using Sable.Tools.Generate.CSharp;
+
+namespace Sable.Compiler.Generate.Analysis
+{
+    public class ReturnAnalysisAdapterBuilder : GenerateVisitor
+    {
+        private ClassElement returnAnalysisAdapter;
+
+        public ReturnAnalysisAdapterBuilder(NameSpaceElement nameElement, PGrammar grammar)
+        {
+            nameElement.CreateClass("ReturnAnalysisAdapter", AccessModifiers.@public, "ReturnAnalysisAdapter<object>");
+
+            returnAnalysisAdapter = nameElement.CreateClass("ReturnAnalysisAdapter", AccessModifiers.@public, "ReturnAdapter<TValue, " + grammar.RootProduction + ">");
+            returnAnalysisAdapter.TypeParameters.Add("TValue");
+        }
+    }
+}
