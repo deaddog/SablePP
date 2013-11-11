47174f851a86a01dedff42591751b718314ae689
deaddog <mikkelslsl@gmail.com>
2013-11-11 21:12:47 +0100

    ReturnAnalysisAdapter code is now generated using builder.

--- SableGrammarParser/Sable.Compiler/Generate/Analysis/AnalysisBuilder.cs
+++ SableGrammarParser/Sable.Compiler/Generate/Analysis/AnalysisBuilder.cs
@@ -12,18 +12,9 @@ namespace Sable.Compiler.Generate.Analysis
     {
         private FileElement fileElement;
         private NameSpaceElement nameElement;
-        private string productionName;
 
-        private PGrammar grammar;
-
-        private ClassElement returnAnalysisAdapter;
-
-        private ClassElement depthFirstReturnAdapter;
-
-        private AnalysisBuilder(PGrammar grammar)
+        private AnalysisBuilder()
         {
-            this.grammar = grammar;
-
             fileElement = new FileElement();
             fileElement.Using.Add("System");
             fileElement.Using.Add("System.Collections.Generic");
@@ -33,32 +24,9 @@ namespace Sable.Compiler.Generate.Analysis
 
         public static FileElement BuildCode(Start<PGrammar> astRoot)
         {
-            AnalysisBuilder n = new AnalysisBuilder(astRoot.Root);
+            AnalysisBuilder n = new AnalysisBuilder();
             n.Visit(astRoot);
             return n.fileElement;
-        }
-
-        private string GetTokenName(PToken element)
-        {
-            if (element is AToken)
-                return "T" + ToCamelCase((element as AToken).Identifier.Text);
-            else
-                throw new NotImplementedException("Unknown token type.");
-        }
-
-        private void CreateReturnAnalysisAdapter()
-        {
-            nameElement.CreateClass("ReturnAnalysisAdapter", AccessModifiers.@public, "ReturnAnalysisAdapter<object>");
-
-            returnAnalysisAdapter = nameElement.CreateClass("ReturnAnalysisAdapter", AccessModifiers.@public, "ReturnAdapter<TValue, " + grammar.RootProduction + ">");
-            returnAnalysisAdapter.TypeParameters.Add("TValue");
-        }
-        private void CreateDepthFirstReturnAdapter()
-        {
-            nameElement.CreateClass("DepthFirstReturnAdapter", AccessModifiers.@public, "DepthFirstReturnAdapter<object>");
-
-            depthFirstReturnAdapter = nameElement.CreateClass("DepthFirstReturnAdapter", AccessModifiers.@public, "ReturnAnalysisAdapter<T>");
-            depthFirstReturnAdapter.TypeParameters.Add("T");
         }
 
         public override void CaseAGrammar(AGrammar node)
@@ -74,69 +42,8 @@ namespace Sable.Compiler.Generate.Analysis
             List<DepthFirstAdapter> adapters = new List<DepthFirstAdapter>();
             adapters.Add(new AnalysisAdapterBuilder(nameElement, node));
             adapters.Add(new DepthFirstAdapterBuilder(nameElement));
+            adapters.Add(new ReturnAnalysisAdapterBuilder(nameElement, node));
             VisitInParallel(node, adapters.ToArray());
-            CreateReturnAnalysisAdapter();
-
-            CreateDepthFirstReturnAdapter();
-
-            if (node.HasAstproductions)
-                Visit(node.Astproductions);
-            else if (node.HasProductions)
-                Visit(node.Productions);
-
-            if (node.HasTokens)
-            {
-                returnAnalysisAdapter.EmitNewLine();
-                Visit(node.Tokens);
-            }
-        }
-
-        public override void CaseAToken(AToken node)
-        {
-            string name = GetTokenName(node);
-
-            EmitCase(name);
-        }
-
-        public override void CaseAAlternative(AAlternative node)
-        {
-            string name = "A" + productionName;
-            if(node.HasAlternativename)
-                name = "A" + ToCamelCase((node.Alternativename as AAlternativename).Name.Text) + productionName;
-
-            EmitCase(name);
-        }
-
-        private void EmitCase(string name)
-        {
-            string caseName = "Case" + name;
-
-            var visitType = returnAnalysisAdapter.CreateMethod(AccessModifiers.@public, "Visit", returnAnalysisAdapter.TypeParameters[0]);
-            visitType.Parameters.Add("node", name);
-            visitType.Parameters.Add("arg", returnAnalysisAdapter.TypeParameters[0]);
-            visitType.EmitReturn();
-            visitType.EmitIdentifier(caseName);
-            using (var par = visitType.EmitParenthesis())
-            {
-                par.EmitIdentifier("node");
-                par.EmitComma();
-                par.EmitIdentifier("arg");
-            }
-            visitType.EmitSemicolon(true);
-
-            var typeMethod = returnAnalysisAdapter.CreateMethod(AccessModifiers.@public | AccessModifiers.@virtual, caseName, returnAnalysisAdapter.TypeParameters[0]);
-            typeMethod.Parameters.Add("node", name);
-            typeMethod.Parameters.Add("arg", returnAnalysisAdapter.TypeParameters[0]);
-
-            typeMethod.EmitReturn();
-            typeMethod.EmitIdentifier("DefaultCase");
-            using (var par = typeMethod.EmitParenthesis())
-            {
-                par.EmitIdentifier("node");
-                par.EmitComma();
-                par.EmitIdentifier("arg");
-            }
-            typeMethod.EmitSemicolon(true);
         }
     }
 }
--- SableGrammarParser/Sable.Compiler/Generate/Analysis/ReturnAnalysisAdapterBuilder.cs
+++ SableGrammarParser/Sable.Compiler/Generate/Analysis/ReturnAnalysisAdapterBuilder.cs
@@ -16,5 +16,57 @@ namespace Sable.Compiler.Generate.Analysis
             returnAnalysisAdapter = nameElement.CreateClass("ReturnAnalysisAdapter", AccessModifiers.@public, "ReturnAdapter<TValue, " + grammar.RootProduction + ">");
             returnAnalysisAdapter.TypeParameters.Add("TValue");
         }
+
+        public override void CaseAGrammar(AGrammar node)
+        {
+            if (node.HasAstproductions)
+                Visit(node.Astproductions);
+            else if (node.HasProductions)
+                Visit(node.Productions);
+
+            if (node.HasTokens)
+                Visit(node.Tokens);
+        }
+
+        public override void CaseAToken(AToken node)
+        {
+            EmitCase("T" + node.Identifier.Text.ToCamelCase());
+        }
+
+        public override void CaseAAlternative(AAlternative node)
+        {
+            EmitCase(node.ClassName);
+        }
+        private void EmitCase(string name)
+        {
+            string caseName = "Case" + name;
+
+            var visitType = returnAnalysisAdapter.CreateMethod(AccessModifiers.@public, "Visit", returnAnalysisAdapter.TypeParameters[0]);
+            visitType.Parameters.Add("node", name);
+            visitType.Parameters.Add("arg", returnAnalysisAdapter.TypeParameters[0]);
+            visitType.EmitReturn();
+            visitType.EmitIdentifier(caseName);
+            using (var par = visitType.EmitParenthesis())
+            {
+                par.EmitIdentifier("node");
+                par.EmitComma();
+                par.EmitIdentifier("arg");
+            }
+            visitType.EmitSemicolon(true);
+
+            var typeMethod = returnAnalysisAdapter.CreateMethod(AccessModifiers.@public | AccessModifiers.@virtual, caseName, returnAnalysisAdapter.TypeParameters[0]);
+            typeMethod.Parameters.Add("node", name);
+            typeMethod.Parameters.Add("arg", returnAnalysisAdapter.TypeParameters[0]);
+
+            typeMethod.EmitReturn();
+            typeMethod.EmitIdentifier("DefaultCase");
+            using (var par = typeMethod.EmitParenthesis())
+            {
+                par.EmitIdentifier("node");
+                par.EmitComma();
+                par.EmitIdentifier("arg");
+            }
+            typeMethod.EmitSemicolon(true);
+        }
     }
 }
