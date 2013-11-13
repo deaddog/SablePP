1fd9463b7ce22647b67e58f1aba441e7159fff49
deaddog <mikkelslsl@gmail.com>
2013-11-13 07:47:44 +0100

    Copied Default-method code generation from DepthFirstAdapterBuilder to ReturnDepthFirstAdapterBuilder.

--- SableGrammarParser/Sable.Compiler/Generate/Analysis/DepthFirstReturnAdapterBuilder.cs
+++ SableGrammarParser/Sable.Compiler/Generate/Analysis/DepthFirstReturnAdapterBuilder.cs
@@ -26,6 +26,18 @@ namespace Sable.Compiler.Generate.Analysis
 
         public override void CaseAGrammar(AGrammar node)
         {
+            this.grammar = node;
+
+            CreateVisitNodeMethod();
+            adapterClass.EmitNewLine();
+
+            CreateStartInOutMethods();
+            CreateStartCaseMethod();
+            adapterClass.EmitNewLine();
+
+            CreateDefaultProductionMethods();
+            CreateDefaultAlternativeMethods();
+
             if (node.HasAstproductions)
                 Visit(node.Astproductions);
             else if (node.HasProductions)
@@ -34,5 +46,98 @@ namespace Sable.Compiler.Generate.Analysis
             if (node.HasTokens)
                 Visit(node.Tokens);
         }
+
+        #region Default methods
+
+        public void CreateVisitNodeMethod()
+        {
+            var method = adapterClass.CreateMethod(AccessModifiers.@public | AccessModifiers.@override, "Visit", "void");
+            method.Parameters.Add("node", "Node");
+            method.EmitIdentifier("Visit");
+            using (var par = method.EmitParenthesis())
+            {
+                par.EmitDynamic(true);
+                par.EmitIdentifier("node");
+            }
+            method.EmitSemicolon(true);
+        }
+
+        public void CreateStartInOutMethods()
+        {
+            var inMethod = adapterClass.CreateMethod(AccessModifiers.@public | AccessModifiers.@virtual, "InStart", "void");
+            inMethod.Parameters.Add("node", "Start<" + grammar.RootProduction + ">");
+
+            var outMethod = adapterClass.CreateMethod(AccessModifiers.@public | AccessModifiers.@virtual, "OutStart", "void");
+            outMethod.Parameters.Add("node", "Start<" + grammar.RootProduction + ">");
+        }
+        public void CreateStartCaseMethod()
+        {
+            var method = adapterClass.CreateMethod(AccessModifiers.@public | AccessModifiers.@override, "CaseStart", "void");
+            method.Parameters.Add("node", "Start<" + grammar.RootProduction + ">");
+
+            method.EmitIdentifier("InStart");
+            using (var par = method.EmitParenthesis())
+                par.EmitIdentifier("node");
+            method.EmitSemicolon(true);
+            method.EmitNewLine();
+
+            if (!reversed)
+            {
+                EmitVisitStartMethodCall(method);
+                EmitVisitEOFMethodCall(method);
+            }
+            else
+            {
+                EmitVisitEOFMethodCall(method);
+                EmitVisitStartMethodCall(method);
+            }
+            method.EmitNewLine();
+
+            method.EmitIdentifier("OutStart");
+            using (var par = method.EmitParenthesis())
+                par.EmitIdentifier("node");
+            method.EmitSemicolon(true);
+        }
+        public void EmitVisitStartMethodCall(MethodElement method)
+        {
+            method.EmitIdentifier("Visit");
+            using (var par = method.EmitParenthesis())
+            {
+                using (var par2 = par.EmitParenthesis())
+                    par2.EmitIdentifier("dynamic");
+                par.EmitIdentifier("node");
+                par.EmitPeriod();
+                par.EmitIdentifier("Root");
+            }
+            method.EmitSemicolon(true);
+        }
+        public void EmitVisitEOFMethodCall(MethodElement method)
+        {
+            method.EmitIdentifier("Visit");
+            using (var par = method.EmitParenthesis())
+            {
+                par.EmitIdentifier("node");
+                par.EmitPeriod();
+                par.EmitIdentifier("EOF");
+            }
+            method.EmitSemicolon(true);
+        }
+
+        public void CreateDefaultProductionMethods()
+        {
+            var inMethod = adapterClass.CreateMethod(AccessModifiers.@public | AccessModifiers.@virtual, "DefaultPIn", "void");
+            inMethod.Parameters.Add("node", "Node");
+            var outMethod = adapterClass.CreateMethod(AccessModifiers.@public | AccessModifiers.@virtual, "DefaultPOut", "void");
+            outMethod.Parameters.Add("node", "Node");
+        }
+        public void CreateDefaultAlternativeMethods()
+        {
+            var inMethod = adapterClass.CreateMethod(AccessModifiers.@public | AccessModifiers.@virtual, "DefaultAIn", "void");
+            inMethod.Parameters.Add("node", "Node");
+            var outMethod = adapterClass.CreateMethod(AccessModifiers.@public | AccessModifiers.@virtual, "DefaultAOut", "void");
+            outMethod.Parameters.Add("node", "Node");
+        }
+
+        #endregion
     }
 }
