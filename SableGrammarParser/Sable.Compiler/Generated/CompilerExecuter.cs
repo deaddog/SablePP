3af61cdfd228a7b2344e4116003804f456e64362
deaddog <mikkelslsl@gmail.com>
2013-11-15 17:16:45 +0100

    Included the auto-generated CompilerExecuter.

--- SableGrammarParser/Sable.Compiler/Generated/CompilerExecuter.cs
+++ SableGrammarParser/Sable.Compiler/Generated/CompilerExecuter.cs
@@ -1,16 +1,14 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 using System.IO;
-
 using Sable.Tools;
 using Sable.Tools.Error;
 using Sable.Tools.Lexing;
 using Sable.Tools.Nodes;
 using Sable.Tools.Parsing;
-
 using Sable.Compiler.Lexing;
 using Sable.Compiler.Nodes;
 using Sable.Compiler.Parsing;
-using System.Diagnostics;
 
 namespace Sable.Compiler
 {
@@ -24,24 +22,24 @@ namespace Sable.Compiler
         {
             return new Lexer(reader);
         }
-
+        
         IParser Sable.Tools.ICompilerExecuter.GetParser(ILexer lexer)
         {
             if (!(lexer is Lexer))
                 throw new ArgumentException("Lexer must be of type " + typeof(Lexer).FullName, "lexer");
-
+            
             return this.GetParser(lexer as Lexer);
         }
         public Parser GetParser(Lexer lexer)
         {
             return new Parser(lexer);
         }
-
+        
         void Sable.Tools.ICompilerExecuter.Validate(Node astRoot, ErrorManager errorManager)
         {
             if (!(astRoot is Start<PGrammar>))
                 throw new ArgumentException("Root must be of type " + typeof(Start<PGrammar>).FullName, "astRoot");
-
+            
             this.Validate(astRoot as Start<PGrammar>, errorManager);
         }
         partial void PerformValidation(Start<PGrammar> root, ErrorManager errorManager);
