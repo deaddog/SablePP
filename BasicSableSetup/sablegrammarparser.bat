270845249e74b74710a810a354621cbc891bd14e
deaddog <mikkelslsl@gmail.com>
2013-11-21 21:21:39 +0100

    Removed grammar example and scripts. Moved compiler grammar and sablecc.jar to root.

--- BasicSableSetup/sablegrammarparser.bat
+++
@@ -1,8 +0,0 @@
-:start
-@cls
-@call sablecc_altgen -d generated -t csharp sablegrammarparser.sablecc
-
-@choice /C YN /N /M "Would you like to run sableCC again?"
-@IF ERRORLEVEL 2 GOTO exit
-@IF ERRORLEVEL 1 GOTO start
-:exit
