de17750282b0b2bf04356e061b1b038316eab101
deaddog <mikkelslsl@gmail.com>
2013-11-25 17:42:38 +0100

    Added highlight rule declaration type to SPP and created a file for implementing symbol linking in highlighting rules.

---
+++ SablePP/SablePP.Compiler/Declarations/DHighlightRule.cs
@@ -0,0 +1,18 @@
+﻿using SablePP.Compiler.Nodes;
+using System;
+
+namespace SablePP.Compiler
+{
+    public class DHighlightRule : Declaration
+    {
+        public DHighlightRule(AHighlightrule node)
+            : base(node.Name)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "S:" + base.ToString();
+        }
+    }
+}
---
+++ SablePP/SablePP.Compiler/SymbolLinking/TokenHighlightVisitor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SablePP.Compiler.Nodes;
+using SablePP.Tools.Error;
+
+namespace SablePP.Compiler.SymbolLinking
+{
+    public class TokenHighlightVisitor : Error.ErrorVisitor
+    {
+        private TokenHighlightVisitor(DeclarationTables declarations, ErrorManager errorManager)
+            : base(errorManager)
+        {
+        }
+
+        public static void LoadTokenDeclarations(PHighlightrules node, DeclarationTables declarations, ErrorManager errorManager)
+        {
+            new TokenHighlightVisitor(declarations, errorManager).Visit(node);
+        }
+    }
+}
