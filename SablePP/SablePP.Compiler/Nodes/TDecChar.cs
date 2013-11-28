5f1793705a45550829515bb25d83e11aef13e31f
deaddog <mikkelslsl@gmail.com>
2013-11-28 09:56:16 +0100

    TDecChar token includes a Value property.

---
+++ SablePP/SablePP.Compiler/Nodes/TDecChar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SablePP.Compiler.Nodes
+{
+    public partial class TDecChar
+    {
+        public int Value
+        {
+            get { return int.Parse(this.Text); }
+        }
+    }
+}
