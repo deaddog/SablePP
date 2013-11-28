d30c4d1a2c4968ac70af42018dc98abc4a0c6248
deaddog <mikkelslsl@gmail.com>
2013-11-28 09:49:37 +0100

    Know styles for a highlight rule are stored temporarely during validation.

--- SablePP/SablePP.Compiler/Validation/SyntaxHighlightValidator.cs
+++ SablePP/SablePP.Compiler/Validation/SyntaxHighlightValidator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using SablePP.Tools.Error;
+using SablePP.Compiler.Nodes;
 
 namespace SablePP.Compiler.Validation
 {
@@ -12,15 +13,46 @@ namespace SablePP.Compiler.Validation
         {
         }
 
-        public override void CaseAGrammar(Nodes.AGrammar node)
+        public override void CaseAGrammar(AGrammar node)
         {
             if (node.HasHighlightrules)
                 Visit((dynamic)node.Highlightrules);
         }
 
-        public override void CaseAHighlightrule(Nodes.AHighlightrule node)
+        public override void CaseAHighlightrule(AHighlightrule node)
         {
+            bold = null;
+            italic = null;
+            text = null;
+            background = null;
+
             base.CaseAHighlightrule(node);
+        }
+
+        private ABoldHighlightStyle bold = null;
+        private AItalicHighlightStyle italic = null;
+        private ATextHighlightStyle text = null;
+        private ABackgroundHighlightStyle background = null;
+
+        public override void CaseABoldHighlightStyle(ABoldHighlightStyle node)
+        {
+            bold = node;
+            base.CaseABoldHighlightStyle(node);
+        }
+        public override void CaseAItalicHighlightStyle(AItalicHighlightStyle node)
+        {
+            italic = node;
+            base.CaseAItalicHighlightStyle(node);
+        }
+        public override void CaseATextHighlightStyle(ATextHighlightStyle node)
+        {
+            text = node;
+            base.CaseATextHighlightStyle(node);
+        }
+        public override void CaseABackgroundHighlightStyle(ABackgroundHighlightStyle node)
+        {
+            background = node;
+            base.CaseABackgroundHighlightStyle(node);
         }
     }
 }
