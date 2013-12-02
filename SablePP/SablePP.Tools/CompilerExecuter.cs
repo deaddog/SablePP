f64cc7091914f12f6f0afbc7709fe7a1c3afc9ef
deaddog <mikkelslsl@gmail.com>
2013-12-02 18:01:56 +0100

    Removed the non-generic CompilerExecuter class from tools.

--- SablePP/SablePP.Tools/CompilerExecuter.cs
+++ SablePP/SablePP.Tools/CompilerExecuter.cs
@@ -10,66 +10,13 @@ using FastColoredTextBoxNS;
 
 namespace SablePP.Tools
 {
-    public abstract class CompilerExecuter : ICompilerExecuter
-    {
-        internal CompilerExecuter()
-        {
-        }
-
-        internal protected abstract ILexer getLexer(TextReader reader);
-        internal protected abstract IParser getParser(ILexer lexer);
-        internal protected abstract void validate(Node astRoot, ErrorManager errorManager);
-        internal protected abstract Style getSimpleStyle(Token token);
-
-        ILexer ICompilerExecuter.GetLexer(TextReader reader)
-        {
-            return getLexer(reader);
-        }
-        IParser ICompilerExecuter.GetParser(ILexer lexer)
-        {
-            return getParser(lexer);
-        }
-        void ICompilerExecuter.Validate(Node astRoot, ErrorManager errorManager)
-        {
-            validate(astRoot, errorManager);
-        }
-        Style ICompilerExecuter.GetSimpleStyle(Token token)
-        {
-            return getSimpleStyle(token);
-        }
-    }
-
-    public abstract class CompilerExecuter<TRoot, TLexer, TParser> : CompilerExecuter
+    public abstract class CompilerExecuter<TRoot, TLexer, TParser> : ICompilerExecuter
         where TRoot : Node
         where TLexer : class, ILexer
         where TParser : class, IParser
     {
         public CompilerExecuter()
-            : base()
         {
-        }
-
-        protected sealed internal override ILexer getLexer(TextReader reader)
-        {
-            return GetLexer(reader);
-        }
-        protected sealed internal override IParser getParser(ILexer lexer)
-        {
-            if (!(lexer is TLexer || (lexer is ResetableLexer && (lexer as ResetableLexer).InnerLexer is TLexer)))
-                throw new ArgumentException("Lexer must be of type " + typeof(TLexer).FullName, "lexer");
-
-            return this.GetParser(lexer);
-        }
-        protected sealed internal override void validate(Node astRoot, ErrorManager errorManager)
-        {
-            if (!(astRoot is Start<TRoot>))
-                throw new ArgumentException("Root must be of type " + typeof(Start<TRoot>).FullName, "astRoot");
-
-            this.Validate(astRoot as Start<TRoot>, errorManager);
-        }
-        protected sealed internal override Style getSimpleStyle(Token token)
-        {
-            return GetSimpleStyle(token);
         }
 
         public abstract TLexer GetLexer(TextReader reader);
@@ -78,5 +25,28 @@ namespace SablePP.Tools
         {
         }
         public abstract Style GetSimpleStyle(Token token);
+
+        ILexer ICompilerExecuter.GetLexer(TextReader reader)
+        {
+            return this.GetLexer(reader);
+        }
+        IParser ICompilerExecuter.GetParser(ILexer lexer)
+        {
+            if (!(lexer is TLexer || (lexer is ResetableLexer && (lexer as ResetableLexer).InnerLexer is TLexer)))
+                throw new ArgumentException("Lexer must be of type " + typeof(TLexer).FullName, "lexer");
+
+            return this.GetParser(lexer);
+        }
+        void ICompilerExecuter.Validate(Node astRoot, ErrorManager errorManager)
+        {
+            if (!(astRoot is Start<TRoot>))
+                throw new ArgumentException("Root must be of type " + typeof(Start<TRoot>).FullName, "astRoot");
+
+            this.Validate(astRoot as Start<TRoot>, errorManager);
+        }
+        Style ICompilerExecuter.GetSimpleStyle(Token token)
+        {
+            return this.GetSimpleStyle(token);
+        }
     }
 }
