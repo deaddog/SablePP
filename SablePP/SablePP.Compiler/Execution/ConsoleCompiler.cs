21c3dd9de8f790222392ea147bdbc9d1396f6270
deaddog <mikkelslsl@gmail.com>
2013-12-02 19:51:43 +0100

    Moved Console implementation to ConsoleCompiler.

--- SablePP/SablePP.Compiler/Execution/ConsoleCompiler.cs
+++ SablePP/SablePP.Compiler/Execution/ConsoleCompiler.cs
@@ -1,6 +1,7 @@
 ﻿using SablePP.Compiler.Lexing;
 using SablePP.Compiler.Nodes;
 using SablePP.Compiler.Parsing;
+using SablePP.Tools.Error;
 using SablePP.Tools.Nodes;
 using System;
 using System.IO;
@@ -11,7 +12,40 @@ namespace SablePP.Compiler.Execution
     {
         protected override void Main()
         {
-            throw new NotImplementedException();
+            string inputGrammar = Arguments["grammar"][0];
+            File.Copy(inputGrammar, PathInformation.TemporaryGrammarPath, true);
+
+            string outputDirectory = Arguments["out"][0];
+            outputDirectory = outputDirectory.TrimEnd('\\');
+
+            CompilerExecuter executer = new CompilerExecuter(true);
+
+            Console.WriteLine("Input:\n" + inputGrammar + "\n\nValidating grammar.");
+            Start<PGrammar> ast = Parse(ReadFile(PathInformation.TemporaryGrammarPath), executer);
+
+            ErrorManager errorManager = new ErrorManager();
+            executer.Validate(ast, errorManager);
+
+            if (errorManager.Count > 0)
+            {
+                Console.WriteLine("Validation failed:");
+                foreach(var err in errorManager)
+                    Console.WriteLine(err);
+#if DEBUG
+                Console.ReadKey(true);
+#endif
+                return;
+            }
+            else
+                Console.WriteLine("Validated.\n");
+
+            Console.WriteLine("Starting Post-Sable Process.");
+            executer.Generate(ast, outputDirectory);
+
+            Console.WriteLine("Done.");
+#if DEBUG
+            Console.ReadKey(true);
+#endif
         }
 
         private static string ReadFile(string filepath)
