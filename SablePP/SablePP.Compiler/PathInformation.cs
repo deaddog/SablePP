e8d913025a8b3d3df2a5981fd978435bf160b937
deaddog <mikkelslsl@gmail.com>
2013-12-04 20:10:06 +0100

    Updated the code that tests for Java installation in registry. Now tests for both 32 and 64 bit.

--- SablePP/SablePP.Compiler/PathInformation.cs
+++ SablePP/SablePP.Compiler/PathInformation.cs
@@ -64,21 +64,44 @@ namespace SablePP.Compiler
                 dir.Create();
         }
 
+        private static bool _javaLoaded_ = false;
+        private static void loadJavaInformation()
+        {
+            if (_javaLoaded_)
+                return;
+
+            if (!loadJavaFrom(RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64)))
+                loadJavaFrom(RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32));
+
+            _javaLoaded_ = true;
+        }
+
+        private static bool loadJavaFrom(RegistryKey rootKey)
+        {
+            bool result = false;
+
+            if (rootKey != null)
+                using (var baseKey = rootKey.OpenSubKey("SOFTWARE\\JavaSoft\\Java Runtime Environment"))
+                    if (baseKey != null)
+                    {
+                        string currentVersion = baseKey.GetValue("CurrentVersion").ToString();
+                        using (var homeKey = baseKey.OpenSubKey(currentVersion))
+                        {
+                            _javaVersion_ = currentVersion;
+                            _javaDirectory_ = homeKey.GetValue("JavaHome").ToString();
+                            _javaFound_ = true;
+                        }
+                    }
+
+            return result;
+        }
+
         private static string _javaDirectory_ = null;
         private static string javaDirectory
         {
             get
             {
-                if (_javaDirectory_ == null)
-                {
-                    string javaKey = "SOFTWARE\\JavaSoft\\Java Runtime Environment";
-                    using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(javaKey))
-                    {
-                        string currentVersion = baseKey.GetValue("CurrentVersion").ToString();
-                        using (var homeKey = baseKey.OpenSubKey(currentVersion))
-                            _javaDirectory_ = homeKey.GetValue("JavaHome").ToString();
-                    }
-                }
+                loadJavaInformation();
                 return _javaDirectory_;
             }
         }
@@ -92,15 +115,18 @@ namespace SablePP.Compiler
         {
             get
             {
-                if (_javaVersion_ == null)
-                {
-                    string javaKey = "SOFTWARE\\JavaSoft\\Java Runtime Environment";
-                    using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(javaKey))
-                    {
-                        _javaVersion_ = baseKey.GetValue("CurrentVersion").ToString();
-                    }
-                }
+                loadJavaInformation();
                 return _javaVersion_;
+            }
+        }
+
+        private static bool _javaFound_;
+        public static bool JavaFound
+        {
+            get
+            {
+                loadJavaInformation();
+                return _javaFound_;
             }
         }
     }
