bc66b0bf9dc0bc3f4b44f1bbdb1c0109619831ee
deaddog <mikkelslsl@gmail.com>
2014-02-05 13:55:40 +0100

    Token and Production identifiers (in SablePP) are highlighted using rules defined by IdentifierHighlighter.

--- SablePP/SablePP.Compiler/Execution/IdentifierHighlighter.cs
+++ SablePP/SablePP.Compiler/Execution/IdentifierHighlighter.cs
@@ -1,11 +1,15 @@
 ﻿using FastColoredTextBoxNS;
 using SablePP.Compiler.Nodes;
 using SablePP.Tools;
+using System.Drawing;
 
 namespace SablePP.Compiler.Execution
 {
     public class IdentifierHighlighter : IHighlighter
     {
+        private TextStyle tokenStyle = new TextStyle(new SolidBrush(Color.FromArgb(43, 145, 175)), null, FontStyle.Regular);
+        private TextStyle productionStyle = new TextStyle(new SolidBrush(Color.Blue), null, FontStyle.Regular);
+
         Style IHighlighter.GetStyle(Tools.Nodes.Token token)
         {
             if (token is TIdentifier)
@@ -16,6 +20,10 @@ namespace SablePP.Compiler.Execution
 
         private Style getStyle(TIdentifier identifier)
         {
+            if (identifier.IsToken)
+                return tokenStyle;
+            else if (identifier.IsProduction)
+                return productionStyle;
             return null;
         }
     }
