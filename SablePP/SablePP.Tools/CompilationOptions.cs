19fc1b4a3778aeefbf5e4c4681fde0f0d13e22c6
deaddog <mikkelslsl@gmail.com>
2014-02-05 10:29:45 +0100

    Removed the type-argument from CompilationOptions. The AST root is no longer part of the option set.

--- SablePP/SablePP.Tools/CompilationOptions.cs
+++ SablePP/SablePP.Tools/CompilationOptions.cs
@@ -4,22 +4,15 @@ using System;
 
 namespace SablePP.Tools
 {
-    public class CompilationOptions<T>
-        where T : Node
+    public class CompilationOptions
     {
-        private Start<T> root;
         private ErrorManager errorManager;
 
-        public CompilationOptions(Start<T> root, ErrorManager errorManager)
+        public CompilationOptions(ErrorManager errorManager)
         {
-            this.root = root;
             this.errorManager = errorManager;
         }
 
-        public Start<T> Root
-        {
-            get { return root; }
-        }
         public ErrorManager ErrorManager
         {
             get { return errorManager; }
