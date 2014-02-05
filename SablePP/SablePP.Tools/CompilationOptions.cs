50ca8bcaf86bd7fd7bf8defce7dc6cbdd47da17a
deaddog <mikkelslsl@gmail.com>
2014-02-05 08:04:48 +0100

    Created a CompilationOptions class, which will replace the validation arguments in the CompilerExecuter.

---
+++ SablePP/SablePP.Tools/CompilationOptions.cs
@@ -0,0 +1,28 @@
+﻿using SablePP.Tools.Error;
+using SablePP.Tools.Nodes;
+using System;
+
+namespace SablePP.Tools
+{
+    public class CompilationOptions<T>
+        where T : Node
+    {
+        private Start<T> root;
+        private ErrorManager errorManager;
+
+        public CompilationOptions(Start<T> root, ErrorManager errorManager)
+        {
+            this.root = root;
+            this.errorManager = errorManager;
+        }
+
+        public Start<T> Root
+        {
+            get { return root; }
+        }
+        public ErrorManager ErrorManager
+        {
+            get { return errorManager; }
+        }
+    }
+}
