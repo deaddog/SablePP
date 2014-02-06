d55d684983f8b250eafbe5ba68f2ec35de85d15d
deaddog <mikkelslsl@gmail.com>
2014-02-06 21:47:50 +0100

    Added documentation for ILexer, IParser and ResetableLexer.

--- SablePP/SablePP.Tools/Lexing/ILexer.cs
+++ SablePP/SablePP.Tools/Lexing/ILexer.cs
@@ -2,9 +2,20 @@
 
 namespace SablePP.Tools.Lexing
 {
+    /// <summary>
+    /// Defines methods for performing lexical analysis.
+    /// </summary>
     public interface ILexer
     {
+        /// <summary>
+        /// Gets the next <see cref="Token"/> in the token stream, but does not remove it from the stream.
+        /// </summary>
+        /// <returns>The next <see cref="Token"/> in the token stream.</returns>
         Token Peek();
+        /// <summary>
+        /// Gets the next <see cref="Token"/> in the token stream, and removes it from the stream.
+        /// </summary>
+        /// <returns>The next <see cref="Token"/> in the token stream.</returns>
         Token Next();
     }
 }
