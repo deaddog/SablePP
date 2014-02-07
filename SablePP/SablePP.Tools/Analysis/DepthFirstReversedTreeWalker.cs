9dbce4fd335bcaad8b3f59296c56e255143c2757
deaddog <mikkelslsl@gmail.com>
2014-02-07 21:40:30 +0100

    Added documentation for the DepthFirstTreeWalker class and the reversed.

--- SablePP/SablePP.Tools/Analysis/DepthFirstReversedTreeWalker.cs
+++ SablePP/SablePP.Tools/Analysis/DepthFirstReversedTreeWalker.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Linq;
-
+﻿using System.Linq;
 using SablePP.Tools.Nodes;
 
 namespace SablePP.Tools.Analysis
 {
+    /// <summary>
+    /// Represents a <see cref="TreeWalker"/> that performs reverse-ordered depth first traversal of an AST.
+    /// </summary>
     public class DepthFirstReversedTreeWalker : TreeWalker
     {
+        /// <summary>
+        /// When overridden in a derived class, performs tasks related to a single <see cref="Token" /> by 'visiting' it.
+        /// </summary>
+        /// <param name="token">The <see cref="Token" /> to visit.</param>
         public override void Visit(Token token)
         {
         }
+        /// <summary>
+        /// Visits, in reverse order, the children of a <see cref="Production"/> node. This call is made recursively.
+        /// </summary>
+        /// <param name="production">The <see cref="Production" /> whose children to visit.</param>
         public override void Visit(Production production)
         {
             foreach (Node node in production.GetChildren().Reverse())
--- SablePP/SablePP.Tools/Analysis/DepthFirstTreeWalker.cs
+++ SablePP/SablePP.Tools/Analysis/DepthFirstTreeWalker.cs
@@ -1,17 +1,26 @@
-﻿using System;
-
-using SablePP.Tools.Nodes;
+﻿using SablePP.Tools.Nodes;
 
 namespace SablePP.Tools.Analysis
 {
+    /// <summary>
+    /// Represents a <see cref="TreeWalker"/> that performs in-order depth first traversal of an AST.
+    /// </summary>
     public class DepthFirstTreeWalker : TreeWalker
     {
+        /// <summary>
+        /// When overridden in a derived class, performs tasks related to a single <see cref="Token" /> by 'visiting' it.
+        /// </summary>
+        /// <param name="token">The <see cref="Token" /> to visit.</param>
         public override void Visit(Token token)
         {
         }
+        /// <summary>
+        /// Visits, in order, the children of a <see cref="Production"/> node. This call is made recursively.
+        /// </summary>
+        /// <param name="production">The <see cref="Production" /> whose children to visit.</param>
         public override void Visit(Production production)
         {
-            foreach (Nodes.Node node in production.GetChildren())
+            foreach (Node node in production.GetChildren())
                 Visit(node);
         }
     }
