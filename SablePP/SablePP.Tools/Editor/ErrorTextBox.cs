ab6c4358193a784a4847323d5455f33063999b41
deaddog <mikkelslsl@gmail.com>
2014-02-07 22:31:31 +0100

    Added documentation for the ErrorTextBox control.

--- SablePP/SablePP.Tools/Editor/ErrorTextBox.cs
+++ SablePP/SablePP.Tools/Editor/ErrorTextBox.cs
@@ -6,13 +6,23 @@ using SablePP.Tools.Error;
 
 namespace SablePP.Tools.Editor
 {
+    /// <summary>
+    /// A very simple textbox capable of displaying a list of compiler errors.
+    /// </summary>
     public class ErrorTextBox : FastColoredTextBox
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorTextBox"/> class.
+        /// </summary>
         public ErrorTextBox()
             : base()
         {
         }
 
+        /// <summary>
+        /// Adds a <see cref="CompilerError"/> error to the <see cref="ErrorTextBox"/>.
+        /// </summary>
+        /// <param name="error">The error that should be added to the <see cref="ErrorTextBox"/>.</param>
         public void AddError(CompilerError error)
         {
             string text;
@@ -27,6 +37,9 @@ namespace SablePP.Tools.Editor
             else
                 this.Text += "\r\n" + text;
         }
+        /// <summary>
+        /// Clears all errors from the <see cref="ErrorTextBox"/>.
+        /// </summary>
         public void ClearErrors()
         {
             this.Text = "";
