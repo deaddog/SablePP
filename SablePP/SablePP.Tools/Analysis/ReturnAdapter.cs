ec8989ca1117160181b1a8acb48f066067584d1d
deaddog <mikkelslsl@gmail.com>
2014-02-07 22:04:59 +0100

    For 'orthogonality' a VisitInParallel method has been implemented in the ReturnAdapter such that it resembles the Adapter and TreeWalker in the aspect.

--- SablePP/SablePP.Tools/Analysis/ReturnAdapter.cs
+++ SablePP/SablePP.Tools/Analysis/ReturnAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using SablePP.Tools.Nodes;
 
 namespace SablePP.Tools.Analysis
@@ -33,6 +34,31 @@ namespace SablePP.Tools.Analysis
         public virtual T CaseEOF(EOF node, T arg)
         {
             return DefaultCase(node, arg);
+        }
+
+        /// <summary>
+        /// Visits a node (and possibly substructure) using multiple instances of <see cref="Adapter{TValue,TRoot}"/> in parallel.
+        /// The method returns when all adapters have completed execution.
+        /// </summary>
+        /// <param name="node">The node to visit.</param>
+        /// <param name="arg">The <typeparamref name="T"/> argument that is passed to all adapters.</param>
+        /// <param name="adapters">The collection of <see cref="ReturnAdapter{T,TRoot}"/> on which the Visit method should be invoked.</param>
+        /// <returns>An array consisting of the results of all the adapters. The results are ordered in the same order as <paramref name="adapters"/>.</returns>
+        public static T[] VisitInParallel(Node node, T arg, params ReturnAdapter<T, TRoot>[] adapters)
+        {
+            Thread[] threads = new Thread[adapters.Length];
+            T[] results = new T[adapters.Length];
+            for (int i = 0; i < adapters.Length; i++)
+            {
+                ReturnAdapter<T, TRoot> adapter = adapters[i];
+                int index = i;
+                threads[index] = new Thread(() => results[index] = adapter.Visit(node, arg));
+                threads[index].Start();
+            }
+            for (int i = 0; i < threads.Length; i++)
+                threads[i].Join();
+
+            return results;
         }
     }
 }
