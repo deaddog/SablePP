ec24f61ab038e24e8f6efe06df1ca1b81a0736b5
deaddog <mikkelslsl@gmail.com>
2014-02-07 21:30:20 +0100

    Added documentation for the TreeWalker class.

--- SablePP/SablePP.Tools/Analysis/TreeWalker.cs
+++ SablePP/SablePP.Tools/Analysis/TreeWalker.cs
@@ -5,10 +5,26 @@ using SablePP.Tools.Nodes;
 
 namespace SablePP.Tools.Analysis
 {
+    /// <summary>
+    /// Represents a class that allows the traversal of an AST regardless of the associated language.
+    /// This functions as a generalized implementation of the visitor pattern.
+    /// </summary>
     public abstract class TreeWalker
     {
+        /// <summary>
+        /// When overridden in a derived class, performs tasks related to a single <see cref="Token"/> by 'visiting' it.
+        /// </summary>
+        /// <param name="token">The <see cref="Token"/> to visit.</param>
         public abstract void Visit(Token token);
+        /// <summary>
+        /// When overridden in a derived class, performs tasks related to a single <see cref="Production"/> by 'visiting' it.
+        /// </summary>
+        /// <param name="production">The <see cref="Production"/> to visit.</param>
         public abstract void Visit(Production production);
+        /// <summary>
+        /// Visits a <see cref="Node"/> by calling the <see cref="Token"/> or <see cref="Production"/> specific Visit method.
+        /// </summary>
+        /// <param name="node">The <see cref="Node"/> to visit.</param>
         public void Visit(Node node)
         {
             if (node is Token)
@@ -17,6 +33,12 @@ namespace SablePP.Tools.Analysis
                 Visit(node as Production);
         }
 
+        /// <summary>
+        /// Visits a node (and possibly substructure) using multiple instances of <see cref="TreeWalker"/> in parallel.
+        /// The method returns when all walkers have completed execution.
+        /// </summary>
+        /// <param name="node">The node to visit.</param>
+        /// <param name="walkers">The collection of <see cref="TreeWalker"/> on which the Visit method should be invoked.</param>
         public static void VisitInParallel(Node node, params TreeWalker[] walkers)
         {
             Thread[] threads = new Thread[walkers.Length];
