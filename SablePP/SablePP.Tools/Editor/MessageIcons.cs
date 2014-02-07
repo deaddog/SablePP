00795590fb19b3f6185d04bf9d5ad6c336105f56
deaddog <mikkelslsl@gmail.com>
2014-02-07 22:29:14 +0100

    Added documentation for the MessageIcons enum.

--- SablePP/SablePP.Tools/Editor/MessageIcons.cs
+++ SablePP/SablePP.Tools/Editor/MessageIcons.cs
@@ -1,7 +1,13 @@
 ﻿namespace SablePP.Tools.Editor
 {
+    /// <summary>
+    /// Icons to display in the bottom of the <see cref="EditorForm"/>.
+    /// </summary>
     public enum MessageIcons
     {
+        /// <summary>
+        /// An accept icon (checkmark).
+        /// </summary>
         Accept
     }
 }
