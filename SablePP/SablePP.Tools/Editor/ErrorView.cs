ae18e559fedd64cc71dd9996b0b68612b9edb69f
deaddog <mikkelslsl@gmail.com>
2014-02-08 15:24:16 +0100

    Added a new control (ErrorView) for displaying error-messages.

---
+++ SablePP/SablePP.Tools/Editor/ErrorView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SablePP.Tools.Editor
+{
+    public class ErrorView : ListView
+    {
+        public ErrorView()
+            : base()
+        {
+        }
+    }
+}
