b64779412f2554cc8f432e89c336464af90c356e
deaddog <mikkelslsl@gmail.com>
2014-02-08 14:58:20 +0100

    Provided documentation for some of the C# code elements.

--- SablePP/SablePP.Tools/Generate/CSharp/ClassElement.cs
+++ SablePP/SablePP.Tools/Generate/CSharp/ClassElement.cs
@@ -3,27 +3,42 @@ using System.Collections.Generic;
 
 namespace SablePP.Tools.Generate.CSharp
 {
+    /// <summary>
+    /// Represents a C# class and exposes methods for emitting fields, methods, properties etc. within the class.
+    /// </summary>
     public class ClassElement : ComplexElement
     {
         private string name;
+        /// <summary>
+        /// Gets the name of the class.
+        /// </summary>
         public string Name
         {
             get { return name; }
         }
 
         private AccessModifiers modifiers;
+        /// <summary>
+        /// Gets the access modifiers associated with the class.
+        /// </summary>
         public AccessModifiers Modifiers
         {
             get { return modifiers; }
         }
 
         private TypeParametersElement typeParameters;
+        /// <summary>
+        /// Gets the collection of type parameters for the class.
+        /// </summary>
         public TypeParametersElement TypeParameters
         {
             get { return typeParameters; }
         }
 
         private string implements;
+        /// <summary>
+        /// Gets a string describing the types this class inherits.
+        /// </summary>
         public string Implements
         {
             get { return implements; }
