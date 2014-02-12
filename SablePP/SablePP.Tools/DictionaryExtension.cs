edbc00dfc85ae65aeb0d82d6df90ecbedaaa4fc2
deaddog <mikkelslsl@gmail.com>
2014-02-12 13:14:01 +0100

    Added documentation for the DictionaryExtension class.

--- SablePP/SablePP.Tools/DictionaryExtension.cs
+++ SablePP/SablePP.Tools/DictionaryExtension.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 
 namespace SablePP.Tools
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="IDictionary{T1,T2}"/>.
+    /// </summary>
     public static class DictionaryExtension
     {
+        /// <summary>
+        /// Gets the value whose key is closest to <paramref name="key"/>.
+        /// This method always return a value, if the dictionary is not empty.
+        /// </summary>
+        /// <typeparam name="T">The type of the values contained by the dictionary.</typeparam>
+        /// <param name="dict">The dictionary form which a value is extracted.</param>
+        /// <param name="key">The key to look for in the dictionary.</param>
+        /// <returns>The value associated with the key that has the closest edit distance to <paramref name="key"/>.</returns>
         public static T GetNearest<T>(this IDictionary<string, T> dict, string key)
         {
             if (dict.ContainsKey(key))
@@ -25,6 +36,15 @@ namespace SablePP.Tools
             return t;
         }
 
+        /// <summary>
+        /// Gets the element that is closest to <paramref name="key"/>.
+        /// This method always return a value, if the dictionary is not empty.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the collection.</typeparam>
+        /// <param name="collection">The collection from which a value is extracted.</param>
+        /// <param name="key">The key to look for in the collection.</param>
+        /// <param name="translate">A method that translates an object of type <typeparamref name="T"/> into a key string..</param>
+        /// <returns>The value associated with the key (translated from objects) that has the closest edit distance to <paramref name="key"/>.</returns>
         public static T GetNearest<T>(this IEnumerable<T> collection, string key, Func<T, string> translate)
         {
             int diff = int.MaxValue;
