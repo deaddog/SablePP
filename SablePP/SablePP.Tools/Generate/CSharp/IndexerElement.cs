8c768ab59cfafd8e0a257222ee957b088cbc0411
deaddog <mikkelslsl@gmail.com>
2014-02-13 08:27:30 +0100

    Implemented the IndexerElement using the same setup as the PropertyElement.

--- SablePP/SablePP.Tools/Generate/CSharp/IndexerElement.cs
+++ SablePP/SablePP.Tools/Generate/CSharp/IndexerElement.cs
@@ -6,66 +6,127 @@ namespace SablePP.Tools.Generate.CSharp
     public class IndexerElement : CSharpElement
     {
         private string type;
-
         public string Type
         {
             get { return type; }
         }
 
-        private AccessModifiers modifiers;
+        private AccessModifierElement modifiers;
         public AccessModifiers Modifiers
         {
-            get { return modifiers; }
+            get { return modifiers.Modifiers; }
+            set { modifiers.Modifiers = value; }
         }
-
+        
         private MethodElement.ParametersElement parameters;
         public MethodElement.ParametersElement Parameters
         {
             get { return parameters; }
         }
 
-        private PropertyElement.PropertyGetter getter;
-        public PropertyElement.PropertyGetter Get
-        {
-            get { return getter; }
-        }
-        private PropertyElement.PropertySetter setter;
-        public PropertyElement.PropertySetter Set
-        {
-            get { return setter; }
-        }
-
-        public IndexerElement(AccessModifiers modifiers, string type, bool hasGetter, bool hasSetter)
+        public IndexerElement(AccessModifiers modifiers, string type)
         {
             if (type == null)
                 throw new ArgumentNullException("type");
             if (type == string.Empty)
                 throw new ArgumentException("Property must have a type.", "type");
 
-            this.modifiers = modifiers;
+            this.modifiers = new AccessModifierElement(modifiers);
             this.type = type.Trim();
 
             this.parameters = new MethodElement.ParametersElement();
 
-            if (hasGetter)
-                this.getter = new PropertyElement.PropertyGetter();
-            if (hasSetter)
-                this.setter = new PropertyElement.PropertySetter();
-
-            modifiers.Emit(emit);
+            insertElement(this.modifiers);
             emit(type, UseSpace.NotPreferred, UseSpace.Always);
             emit("this[", UseSpace.Always, UseSpace.Never);
             insertElement(parameters);
             emit("]", UseSpace.Never, UseSpace.NotPreferred);
+        }
+    }
 
+    public class GetIndexerElement : IndexerElement, IGetIndexer
+    {
+        private GetSetHandle handle;
+
+        public GetIndexerElement(AccessModifiers modifiers, string type)
+            : base(modifiers, type)
+        {
             emitBlockStart();
-
-            if (hasGetter) 
-                insertElement(getter);
-            if (hasSetter)
-                insertElement(setter);
-
+            handle = new GetSetHandle("get");
+            insertElement(handle);
             emitBlockEnd();
+        }
+
+        public PatchElement Get
+        {
+            get { return handle.Content; }
+        }
+
+        public AccessModifiers GetModifiers
+        {
+            get { return handle.Access; }
+            set { handle.Access = value; }
+        }
+    }
+
+    public class SetIndexerElement : IndexerElement, ISetIndexer
+    {
+        private GetSetHandle handle;
+
+        public SetIndexerElement(AccessModifiers modifiers, string type)
+            : base(modifiers, type)
+        {
+            emitBlockStart();
+            handle = new GetSetHandle("set");
+            insertElement(handle);
+            emitBlockEnd();
+        }
+
+        public PatchElement Set
+        {
+            get { return handle.Content; }
+        }
+
+        public AccessModifiers SetModifiers
+        {
+            get { return handle.Access; }
+            set { handle.Access = value; }
+        }
+    }
+
+    public class GetSetIndexerElement : IndexerElement, IGetIndexer, ISetIndexer
+    {
+        private GetSetHandle getter, setter;
+
+        public GetSetIndexerElement(AccessModifiers modifiers, string type)
+            : base(modifiers, type)
+        {
+            emitBlockStart();
+            getter = new GetSetHandle("get");
+            setter = new GetSetHandle("set");
+            insertElement(getter);
+            insertElement(setter);
+            emitBlockEnd();
+        }
+
+        public PatchElement Get
+        {
+            get { return getter.Content; }
+        }
+        public PatchElement Set
+        {
+            get { return setter.Content; }
+        }
+
+        public AccessModifiers GetModifiers
+        {
+            get { return getter.Access; }
+            set { getter.Access = value; }
+        }
+        public AccessModifiers SetModifiers
+        {
+            get { return setter.Access; }
+            set { setter.Access = value; }
         }
     }
 }
