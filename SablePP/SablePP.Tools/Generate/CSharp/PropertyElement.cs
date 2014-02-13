9470e1f76d0b3095a438495408d74af2b5cbadde
deaddog <mikkelslsl@gmail.com>
2014-02-13 08:08:28 +0100

    Rewrote the PropertyElement to use PatchElement for getter and setter, and implemented interfaces for describing if a property is Get, Set or GetSet.

--- SablePP/SablePP.Tools/Generate/CSharp/PropertyElement.cs
+++ SablePP/SablePP.Tools/Generate/CSharp/PropertyElement.cs
@@ -23,18 +23,7 @@ namespace SablePP.Tools.Generate.CSharp
             set { modifiers.Modifiers = value; }
         }
 
-        private PropertyGetter getter;
-        public PropertyGetter Get
-        {
-            get { return getter; }
-        }
-        private PropertySetter setter;
-        public PropertySetter Set
-        {
-            get { return setter; }
-        }
-
-        public PropertyElement(AccessModifiers modifiers, string name, string type, bool hasGetter, bool hasSetter)
+        protected PropertyElement(AccessModifiers modifiers, string name, string type)
         {
             if (name == null)
                 throw new ArgumentNullException("name");
@@ -50,88 +39,166 @@ namespace SablePP.Tools.Generate.CSharp
             this.name = name.Trim();
             this.type = type.Trim();
 
-            if (hasGetter)
-                this.getter = new PropertyGetter();
-            if (hasSetter)
-                this.setter = new PropertySetter();
-
             insertElement(this.modifiers);
             emit(type, UseSpace.NotPreferred, UseSpace.Always);
             emit(name, UseSpace.NotPreferred, UseSpace.Never);
+        }
 
+        protected class GetSetHandle : ComplexElement
+        {
+            private PatchElement break1, break2, break3;
+            private bool hasNewline;
+
+            private PatchElement content;
+            private AccessModifierElement access;
+
+            public PatchElement Content
+            {
+                get { return content; }
+            }
+            public AccessModifiers Access
+            {
+                get { return access.Modifiers; }
+                set { access.Modifiers = value; }
+            }
+
+            public GetSetHandle(string getset)
+            {
+                break1 = new PatchElement();
+                break2 = new PatchElement();
+                break3 = new PatchElement();
+                hasNewline = false;
+
+                content = new PatchElement();
+                content.NewLineEmitted += this.onNewLineEmitted;
+
+                access = new AccessModifierElement(AccessModifiers.None);
+                insertElement(access);
+                emit(getset, UseSpace.Never, UseSpace.Preferred);
+                insertElement(break1);
+                emit("{", UseSpace.NotPreferred, UseSpace.Preferred);
+                insertElement(break2);
+                insertElement(content);
+                insertElement(break3);
+                emit("}", UseSpace.NotPreferred, UseSpace.NotPreferred);
+                emitNewLine();
+            }
+
+            private void onNewLineEmitted()
+            {
+                if (!hasNewline)
+                {
+                    hasNewline = true;
+                    break1.EmitNewLine();
+                    break2.EmitNewLine();
+                    break2.IncreaseIndentation();
+                    break3.DecreaseIndentation();
+                }
+            }
+        }
+    }
+
+    public class GetPropertyElement : PropertyElement, IGetProperty
+    {
+        private GetSetHandle handle;
+
+        public GetPropertyElement(AccessModifiers modifiers, string name, string type)
+            : base(modifiers, name, type)
+        {
             emitBlockStart();
-
-            if (hasGetter)
-                insertElement(getter);
-            if (hasSetter)
-                insertElement(setter);
-
+            handle = new GetSetHandle("get");
+            insertElement(handle);
             emitBlockEnd();
         }
 
-        public class PropertyGetter : ExecutableElement
+        public PatchElement Get
         {
-            private PatchElement break1 = new PatchElement(), break2 = new PatchElement(), break3 = new PatchElement();
-            private bool hasbreak = false;
-
-            public PropertyGetter()
-            {
-                emit("get", UseSpace.Never, UseSpace.Preferred);
-                insertElement(break1);
-                emit("{", UseSpace.NotPreferred, UseSpace.Preferred);
-                insertElement(break2);
-                InsertContents();
-                insertElement(break3);
-                emit("}", UseSpace.NotPreferred, UseSpace.NotPreferred);
-                emitNewLine();
-            }
-
-            protected override void OnNewLineEmitted()
-            {
-                if (!hasbreak)
-                {
-                    hasbreak = true;
-                    break1.EmitNewLine();
-                    break2.EmitNewLine();
-                    break2.IncreaseIndentation();
-                    break3.DecreaseIndentation();
-                }
-            }
+            get { return handle.Content; }
         }
 
-        public class PropertySetter : ExecutableElement
+        public AccessModifiers GetModifiers
         {
-            private PatchElement break1 = new PatchElement(), break2 = new PatchElement(), break3 = new PatchElement();
-            private bool hasbreak = false;
-
-            public PropertySetter()
-            {
-                emit("set", UseSpace.Never, UseSpace.Preferred);
-                insertElement(break1);
-                emit("{", UseSpace.NotPreferred, UseSpace.Preferred);
-                insertElement(break2);
-                InsertContents();
-                insertElement(break3);
-                emit("}", UseSpace.NotPreferred, UseSpace.NotPreferred);
-                emitNewLine();
-            }
-
-            protected override void OnNewLineEmitted()
-            {
-                if (!hasbreak)
-                {
-                    hasbreak = true;
-                    break1.EmitNewLine();
-                    break2.EmitNewLine();
-                    break2.IncreaseIndentation();
-                    break3.DecreaseIndentation();
-                }
-            }
-
-            public void EmitValueIdentifier()
-            {
-                EmitIdentifier("value");
-            }
+            get { return handle.Access; }
+            set { handle.Access = value; }
         }
+    }
+
+    public class SetPropertyElement : PropertyElement, ISetProperty
+    {
+        private GetSetHandle handle;
+
+        public SetPropertyElement(AccessModifiers modifiers, string name, string type)
+            : base(modifiers, name, type)
+        {
+            emitBlockStart();
+            handle = new GetSetHandle("set");
+            insertElement(handle);
+            emitBlockEnd();
+        }
+
+        public PatchElement Set
+        {
+            get { return handle.Content; }
+        }
+
+        public AccessModifiers SetModifiers
+        {
+            get { return handle.Access; }
+            set { handle.Access = value; }
+        }
+    }
+
+    public class GetSetPropertyElement : PropertyElement, IGetProperty, ISetProperty
+    {
+        private GetSetHandle getter, setter;
+
+        public GetSetPropertyElement(AccessModifiers modifiers, string name, string type)
+            : base(modifiers, name, type)
+        {
+            emitBlockStart();
+            getter = new GetSetHandle("get");
+            setter = new GetSetHandle("set");
+            insertElement(getter);
+            insertElement(setter);
+            emitBlockEnd();
+        }
+
+        public PatchElement Get
+        {
+            get { return getter.Content; }
+        }
+        public PatchElement Set
+        {
+            get { return setter.Content; }
+        }
+
+        public AccessModifiers GetModifiers
+        {
+            get { return getter.Access; }
+            set { getter.Access = value; }
+        }
+        public AccessModifiers SetModifiers
+        {
+            get { return setter.Access; }
+            set { setter.Access = value; }
+        }
+    }
+
+    public interface IProperty
+    {
+        AccessModifiers Modifiers { get; set; }
+        string Name { get; }
+        string Type { get; }
+    }
+
+    public interface IGetProperty : IProperty
+    {
+        PatchElement Get { get; }
+        AccessModifiers GetModifiers { get; set; }
+    }
+    public interface ISetProperty : IProperty
+    {
+        PatchElement Set { get; }
+        AccessModifiers SetModifiers { get; set; }
     }
 }
