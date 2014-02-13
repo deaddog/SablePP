309eb428fc3f60c6a2ae91ddf2567372ef28a5fe
deaddog <mikkelslsl@gmail.com>
2014-02-13 08:12:45 +0100

    Moved the GetSetHandle to a separate file. It is also no longer a nested class in PropertyElement.

--- SablePP/SablePP.Tools/Generate/CSharp/PropertyElement.cs
+++ SablePP/SablePP.Tools/Generate/CSharp/PropertyElement.cs
@@ -43,59 +43,6 @@ namespace SablePP.Tools.Generate.CSharp
             emit(type, UseSpace.NotPreferred, UseSpace.Always);
             emit(name, UseSpace.NotPreferred, UseSpace.Never);
         }
-
-        protected class GetSetHandle : ComplexElement
-        {
-            private PatchElement break1, break2, break3;
-            private bool hasNewline;
-
-            private PatchElement content;
-            private AccessModifierElement access;
-
-            public PatchElement Content
-            {
-                get { return content; }
-            }
-            public AccessModifiers Access
-            {
-                get { return access.Modifiers; }
-                set { access.Modifiers = value; }
-            }
-
-            public GetSetHandle(string getset)
-            {
-                break1 = new PatchElement();
-                break2 = new PatchElement();
-                break3 = new PatchElement();
-                hasNewline = false;
-
-                content = new PatchElement();
-                content.NewLineEmitted += this.onNewLineEmitted;
-
-                access = new AccessModifierElement(AccessModifiers.None);
-                insertElement(access);
-                emit(getset, UseSpace.Never, UseSpace.Preferred);
-                insertElement(break1);
-                emit("{", UseSpace.NotPreferred, UseSpace.Preferred);
-                insertElement(break2);
-                insertElement(content);
-                insertElement(break3);
-                emit("}", UseSpace.NotPreferred, UseSpace.NotPreferred);
-                emitNewLine();
-            }
-
-            private void onNewLineEmitted()
-            {
-                if (!hasNewline)
-                {
-                    hasNewline = true;
-                    break1.EmitNewLine();
-                    break2.EmitNewLine();
-                    break2.IncreaseIndentation();
-                    break3.DecreaseIndentation();
-                }
-            }
-        }
     }
 
     public class GetPropertyElement : PropertyElement, IGetProperty
