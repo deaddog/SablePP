3b8a9b954181c78bb54714f1bfa93103c70c7561
deaddog <mikkelslsl@gmail.com>
2014-02-13 08:15:01 +0100

    Moved the property interfaces to a separate file.

--- SablePP/SablePP.Tools/Generate/CSharp/PropertyElement.cs
+++ SablePP/SablePP.Tools/Generate/CSharp/PropertyElement.cs
@@ -130,22 +130,4 @@ namespace SablePP.Tools.Generate.CSharp
             set { setter.Access = value; }
         }
     }
-
-    public interface IProperty
-    {
-        AccessModifiers Modifiers { get; set; }
-        string Name { get; }
-        string Type { get; }
-    }
-
-    public interface IGetProperty : IProperty
-    {
-        PatchElement Get { get; }
-        AccessModifiers GetModifiers { get; set; }
-    }
-    public interface ISetProperty : IProperty
-    {
-        PatchElement Set { get; }
-        AccessModifiers SetModifiers { get; set; }
-    }
 }
