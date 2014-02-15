4c04fb9777cb31ef1d9786ff611f2728e11fb853
deaddog <mikkelslsl@gmail.com>
2014-02-15 17:48:50 +0100

    Provided documentation for the NameSpaceElement.

--- SablePP/SablePP.Tools/Generate/CSharp/NameSpaceElement.cs
+++ SablePP/SablePP.Tools/Generate/CSharp/NameSpaceElement.cs
@@ -1,14 +1,23 @@
 ﻿namespace SablePP.Tools.Generate.CSharp
 {
+    /// <summary>
+    /// Represent an object for handling code-generation of a C# namespace.
+    /// </summary>
     public sealed class NameSpaceElement : ComplexElement
     {
         private string name;
+        /// <summary>
+        /// Gets the full name of the namespace.
+        /// </summary>
         public string Name
         {
             get { return name; }
         }
 
         private UsingsElement usings;
+        /// <summary>
+        /// Gets a <see cref="UsingElement"/> describing the using statements included in this <see cref="NameSpaceElement"/>.
+        /// </summary>
         public UsingsElement Using
         {
             get { return usings; }
@@ -16,11 +25,21 @@
 
         private bool hasClasses;
         private PatchElement classes;
+        /// <summary>
+        /// Gets a value indicating whether the namespace contains any <see cref="ClassElement"/>s.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this <see cref="NameSpaceElement"/> contains any <see cref="ClassElement"/>s; otherwise, <c>false</c>.
+        /// </value>
         public bool HasClasses
         {
             get { return hasClasses; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameSpaceElement"/> class.
+        /// </summary>
+        /// <param name="name">The name of the namespace.</param>
         public NameSpaceElement(string name)
         {
             this.name = name;
@@ -37,24 +56,21 @@
             emitLine("}", UseSpace.Never, UseSpace.Never);
         }
 
+        /// <summary>
+        /// Adds a <see cref="ClassElement"/> to the <see cref="NameSpaceElement"/>.
+        /// </summary>
+        /// <param name="class">The class that is added to this <see cref="NameSpaceElement"/>.</param>
         public void Add(ClassElement @class)
         {
             classes.InsertElement(@class);
             hasClasses = true;
         }
 
+        /// <summary>
+        /// Emits a newline to the namespace.
+        /// </summary>
         public void EmitNewLine()
         {
-            classes.EmitNewLine();
-        }
-        public void EmitRegionStart(string text)
-        {
-            classes.Emit("#region " + text, UseSpace.Never, UseSpace.Never);
-            classes.EmitNewLine();
-        }
-        public void EmitRegionEnd()
-        {
-            classes.Emit("#endregion", UseSpace.Never, UseSpace.Never);
             classes.EmitNewLine();
         }
     }
