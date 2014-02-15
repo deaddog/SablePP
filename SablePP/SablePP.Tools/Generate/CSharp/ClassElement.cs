29f1a9459f16a93c3ba8a9cd25f0676d450d6fb6
deaddog <mikkelslsl@gmail.com>
2014-02-15 15:20:07 +0100

    ClassElement properly parses input signature and sets its private fields accordingly.

--- SablePP/SablePP.Tools/Generate/CSharp/ClassElement.cs
+++ SablePP/SablePP.Tools/Generate/CSharp/ClassElement.cs
@@ -17,13 +17,23 @@ namespace SablePP.Tools.Generate.CSharp
             get { return name; }
         }
 
-        private AccessModifiers modifiers;
+        private string classType;
         /// <summary>
-        /// Gets the access modifiers associated with the class.
+        /// Gets the 'class-type' of this element. That is; either 'class', 'struct' or 'interface'.
+        /// </summary>
+        public string ClassType
+        {
+            get { return classType; }
+        }
+
+        private AccessModifierElement modifiers;
+        /// <summary>
+        /// Gets or sets the access modifiers associated with the class.
         /// </summary>
         public AccessModifiers Modifiers
         {
-            get { return modifiers; }
+            get { return modifiers.Modifiers; }
+            set { modifiers.Modifiers = value; }
         }
 
         private TypeParametersElement typeParameters;
@@ -48,6 +58,43 @@ namespace SablePP.Tools.Generate.CSharp
 
         public ClassElement(string signature)
         {
+            if (signature == null)
+                throw new ArgumentNullException("signature");
+            signature = signature.Trim();
+            int start, length;
+
+            // Parse the modifiers
+            this.modifiers = AccessModifierElement.Parse(signature, out start, out length);
+            emit(signature.Substring(0, start));
+            insertElement(modifiers);
+            signature = signature.Substring(start + length).TrimStart();
+
+            // Read class-type
+            this.classType = signature.Substring(0, signature.IndexOf(' '));
+            signature = signature.Substring(classType.Length).TrimStart();
+            emit("{0} ", classType);
+
+            // Find the colon
+            int colonIndex = signature.IndexOf(':');
+            this.name = colonIndex >= 0 ? signature.Substring(0, colonIndex).TrimEnd() : signature;
+            signature = colonIndex >= 0 ? signature.Substring(colonIndex + 1).TrimStart() : string.Empty;
+
+            // Find the type parameters
+            if (name.EndsWith(">"))
+            {
+                int typesStart = name.LastIndexOf('<');
+                string types = name.Substring(typesStart + 1, name.Length - typesStart - 2);
+                typeParameters = TypeParametersElement.Parse(types);
+                name = name.Substring(0, typesStart).TrimEnd();
+            }
+            else
+                typeParameters = TypeParametersElement.Parse(string.Empty);
+
+            // Emit the remainder
+            emit(name);
+            insertElement(typeParameters);
+            if (colonIndex >= 0)
+                emit(" : {0}", signature);
         }
 
         public void EmitNewLine()
