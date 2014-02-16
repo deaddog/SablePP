1e1c30b540b0572b42bc88c94108b18da1d5f9af
deaddog <mikkelslsl@gmail.com>
2014-02-16 22:24:44 +0100

    Added VisitInParallel methods to the ReturnAdapter for two and three arguments.

--- SablePP/SablePP.Tools/Analysis/ReturnAdapter.cs
+++ SablePP/SablePP.Tools/Analysis/ReturnAdapter.cs
@@ -39,19 +39,74 @@ namespace SablePP.Tools.Analysis
         /// The method returns when all adapters have completed execution.
         /// </summary>
         /// <param name="node">The node to visit.</param>
-        /// <param name="arg">The <typeparamref name="T"/> argument that is passed to all adapters.</param>
-        /// <param name="adapters">The collection of <see cref="ReturnAdapter{T,TResult,TRoot}"/> on which the Visit method should be invoked.</param>
+        /// <param name="arg1">The <typeparamref name="T1"/> argument that is passed to all adapters.</param>
+        /// <param name="adapters">The collection of <see cref="ReturnAdapter{T1,TResult,TRoot}"/> on which the Visit method should be invoked.</param>
         /// <returns>An array consisting of the results of all the adapters. The results are ordered in the same order as <paramref name="adapters"/>.</returns>
-        public static TResult[] VisitInParallel<T, TResult, TRoot>(Node node, T arg, params ReturnAdapter<T, TResult, TRoot>[] adapters)
+        public static TResult[] VisitInParallel<T1, TResult, TRoot>(Node node, T1 arg1, params ReturnAdapter<T1, TResult, TRoot>[] adapters)
             where TRoot : Production
         {
             Thread[] threads = new Thread[adapters.Length];
             TResult[] results = new TResult[adapters.Length];
             for (int i = 0; i < adapters.Length; i++)
             {
-                ReturnAdapter<T, TResult, TRoot> adapter = adapters[i];
+                ReturnAdapter<T1, TResult, TRoot> adapter = adapters[i];
                 int index = i;
-                threads[index] = new Thread(() => results[index] = adapter.Visit(node, arg));
+                threads[index] = new Thread(() => results[index] = adapter.Visit(node, arg1));
+                threads[index].Start();
+            }
+            for (int i = 0; i < threads.Length; i++)
+                threads[i].Join();
+
+            return results;
+        }
+
+        /// <summary>
+        /// Visits a node (and possibly substructure) using multiple adapters in parallel.
+        /// The method returns when all adapters have completed execution.
+        /// </summary>
+        /// <param name="node">The node to visit.</param>
+        /// <param name="arg1">The <typeparamref name="T1"/> argument that is passed to all adapters.</param>
+        /// <param name="arg2">The <typeparamref name="T2"/> argument that is passed to all adapters.</param>
+        /// <param name="adapters">The collection of <see cref="ReturnAdapter{T1,T2,TResult,TRoot}"/> on which the Visit method should be invoked.</param>
+        /// <returns>An array consisting of the results of all the adapters. The results are ordered in the same order as <paramref name="adapters"/>.</returns>
+        public static TResult[] VisitInParallel<T1, T2, TResult, TRoot>(Node node, T1 arg1, T2 arg2, params ReturnAdapter<T1, T2, TResult, TRoot>[] adapters)
+            where TRoot : Production
+        {
+            Thread[] threads = new Thread[adapters.Length];
+            TResult[] results = new TResult[adapters.Length];
+            for (int i = 0; i < adapters.Length; i++)
+            {
+                ReturnAdapter<T1, T2, TResult, TRoot> adapter = adapters[i];
+                int index = i;
+                threads[index] = new Thread(() => results[index] = adapter.Visit(node, arg1, arg2));
+                threads[index].Start();
+            }
+            for (int i = 0; i < threads.Length; i++)
+                threads[i].Join();
+
+            return results;
+        }
+
+        /// <summary>
+        /// Visits a node (and possibly substructure) using multiple adapters in parallel.
+        /// The method returns when all adapters have completed execution.
+        /// </summary>
+        /// <param name="node">The node to visit.</param>
+        /// <param name="arg1">The <typeparamref name="T1"/> argument that is passed to all adapters.</param>
+        /// <param name="arg2">The <typeparamref name="T2"/> argument that is passed to all adapters.</param>
+        /// <param name="arg3">The <typeparamref name="T3"/> argument that is passed to all adapters.</param>
+        /// <param name="adapters">The collection of <see cref="ReturnAdapter{T1,T2,TResult,TRoot}"/> on which the Visit method should be invoked.</param>
+        /// <returns>An array consisting of the results of all the adapters. The results are ordered in the same order as <paramref name="adapters"/>.</returns>
+        public static TResult[] VisitInParallel<T1, T2, T3, TResult, TRoot>(Node node, T1 arg1, T2 arg2, T3 arg3, params ReturnAdapter<T1, T2, T3, TResult, TRoot>[] adapters)
+            where TRoot : Production
+        {
+            Thread[] threads = new Thread[adapters.Length];
+            TResult[] results = new TResult[adapters.Length];
+            for (int i = 0; i < adapters.Length; i++)
+            {
+                ReturnAdapter<T1, T2, T3, TResult, TRoot> adapter = adapters[i];
+                int index = i;
+                threads[index] = new Thread(() => results[index] = adapter.Visit(node, arg1, arg2, arg3));
                 threads[index].Start();
             }
             for (int i = 0; i < threads.Length; i++)
@@ -136,13 +191,13 @@ namespace SablePP.Tools.Analysis
     /// <summary>
     /// Defines a base implementation of the visitor pattern in which each Visit method returns a value and takes an argument.
     /// </summary>
-    /// <typeparam name="T">The type of the argument for the Visit methods.</typeparam>
-    /// <typeparam name="TResult">The type of the values that are returned by the <see cref="ReturnAdapter{T,TResult,TRoot}"/></typeparam>
+    /// <typeparam name="T1">The type of the argument for the Visit methods.</typeparam>
+    /// <typeparam name="TResult">The type of the values that are returned by the <see cref="ReturnAdapter{T1,TResult,TRoot}"/></typeparam>
     /// <typeparam name="TRoot">The type of the root production in the AST.</typeparam>
-    public abstract class ReturnAdapter<T, TResult, TRoot> where TRoot : Production
+    public abstract class ReturnAdapter<T1, TResult, TRoot> where TRoot : Production
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ReturnAdapter{T, TResult, TRoot}"/> class.
+        /// Initializes a new instance of the <see cref="ReturnAdapter{T1, TResult, TRoot}"/> class.
         /// </summary>
         public ReturnAdapter()
         {
@@ -152,9 +207,9 @@ namespace SablePP.Tools.Analysis
         /// Visits the specified <see cref="Node"/>.
         /// </summary>
         /// <param name="node">The <see cref="Node"/> that should be visited.</param>
-        /// <param name="arg">The argument passed to the visit handler.</param>
+        /// <param name="arg1">The argument passed to the visit handler.</param>
         /// <returns>A value determined by the visit handler.</returns>
-        public virtual TResult Visit(Node node, T arg)
+        public virtual TResult Visit(Node node, T1 arg1)
         {
             return default(TResult);
         }
@@ -162,9 +217,9 @@ namespace SablePP.Tools.Analysis
         /// When overridden in a derived class, specifies default handler for visiting nodes.
         /// </summary>
         /// <param name="node">The <see cref="Node"/> that should be visited.</param>
-        /// <param name="arg">The argument that is passed to this visit handler.</param>
+        /// <param name="arg1">The argument that is passed to this visit handler.</param>
         /// <returns>A value determined by this visit handler.</returns>
-        public virtual TResult DefaultCase(Node node, T arg)
+        public virtual TResult DefaultCase(Node node, T1 arg1)
         {
             return default(TResult);
         }
@@ -173,42 +228,42 @@ namespace SablePP.Tools.Analysis
         /// Visits the specified <see cref="Start{TRoot}"/> production by calling the <see cref="CaseStart"/> method.
         /// </summary>
         /// <param name="node">The <see cref="Start{TRoot}"/> production that should be visited.</param>
-        /// <param name="arg">The argument passed to the visit handler.</param>
+        /// <param name="arg1">The argument passed to the visit handler.</param>
         /// <returns>A value determined by the visit handler.</returns>
-        public TResult Visit(Start<TRoot> node, T arg)
+        public TResult Visit(Start<TRoot> node, T1 arg1)
         {
-            return CaseStart(node, arg);
+            return CaseStart(node, arg1);
         }
         /// <summary>
         /// Visits the specified <see cref="EOF"/> token by calling the <see cref="CaseEOF"/> method.
         /// </summary>
         /// <param name="node">The <see cref="EOF"/> token that should be visited.</param>
-        /// <param name="arg">The argument passed to the visit handler.</param>
+        /// <param name="arg1">The argument passed to the visit handler.</param>
         /// <returns>A value determined by the visit handler.</returns>
-        public TResult Visit(EOF node, T arg)
+        public TResult Visit(EOF node, T1 arg1)
         {
-            return CaseEOF(node, arg);
+            return CaseEOF(node, arg1);
         }
 
         /// <summary>
         /// When overridden in a derived class, specifies a handler for visiting <see cref="Start{TRoot}"/> production nodes.
         /// </summary>
         /// <param name="node">The <see cref="Start{TRoot}"/> node to handle.</param>
-        /// <param name="arg">The argument that is passed to this visit handler.</param>
+        /// <param name="arg1">The argument that is passed to this visit handler.</param>
         /// <returns>A value determined by this visit handler.</returns>
-        public virtual TResult CaseStart(Start<TRoot> node, T arg)
+        public virtual TResult CaseStart(Start<TRoot> node, T1 arg1)
         {
-            return DefaultCase(node, arg);
+            return DefaultCase(node, arg1);
         }
         /// <summary>
         /// When overridden in a derived class, specifies a handler for visiting <see cref="EOF"/> token nodes.
         /// </summary>
         /// <param name="node">The <see cref="EOF"/> node to handle.</param>
-        /// <param name="arg">The argument that is passed to this visit handler.</param>
+        /// <param name="arg1">The argument that is passed to this visit handler.</param>
         /// <returns>A value determined by this visit handler.</returns>
-        public virtual TResult CaseEOF(EOF node, T arg)
+        public virtual TResult CaseEOF(EOF node, T1 arg1)
         {
-            return DefaultCase(node, arg);
+            return DefaultCase(node, arg1);
         }
     }
 
