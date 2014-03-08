6f3fdef91227f521b79a707399a1fd2d11441fd8
deaddog <mikkelslsl@gmail.com>
2014-03-08 21:37:01 +0100

    Implemented snapping for the LiveCodeEditor such that it can follow its parent form and can 'break free'.

--- SablePP/SablePP.Compiler/Execution/LiveCodeEditor.cs
+++ SablePP/SablePP.Compiler/Execution/LiveCodeEditor.cs
@@ -11,6 +11,10 @@ namespace SablePP.Compiler.Execution
 {
     public partial class LiveCodeEditor : Form
     {
+        private const int POSITION_DISTANCE = 5;
+        private const int SNAPPING_MARGIN = 10;
+        private bool snapped = true;
+
         private Form parentForm;
 
         public Form ParentForm
@@ -39,18 +43,53 @@ namespace SablePP.Compiler.Execution
         {
             base.OnLoad(e);
             if (parentForm != null)
+            {
+                snapped = true;
                 updateBounds(parentForm, EventArgs.Empty);
+            }
         }
 
         void updateBounds(object sender, EventArgs e)
         {
             Form form = sender as Form;
-            this.DesktopBounds = new Rectangle(
-                form.DesktopLocation.X + 5 + form.Width,
-                form.DesktopLocation.Y,
-                this.Width,
-                form.Height
-                );
+            if (snapped)
+            {
+                doingStuff = true;
+
+                this.DesktopBounds = new Rectangle(
+                    form.DesktopLocation.X + POSITION_DISTANCE + form.Width,
+                    form.DesktopLocation.Y,
+                    this.Width,
+                    form.Height
+                    );
+
+                doingStuff = false;
+            }
+        }
+
+        private bool doingStuff = false;
+        protected override void OnLocationChanged(EventArgs e)
+        {
+            base.OnLocationChanged(e);
+            if (parentForm == null)
+                return;
+
+            if (!doingStuff)
+            {
+                doingStuff = true;
+                int diffX = this.DesktopLocation.X - (parentForm.DesktopLocation.X + parentForm.Width + POSITION_DISTANCE);
+                int diffY = this.DesktopLocation.Y - parentForm.DesktopLocation.Y;
+                if (diffX <= SNAPPING_MARGIN && diffX >= -SNAPPING_MARGIN && diffY <= SNAPPING_MARGIN && diffY >= -SNAPPING_MARGIN)
+                {
+                    this.DesktopLocation = new Point(
+                        parentForm.DesktopLocation.X + parentForm.Width + POSITION_DISTANCE,
+                        parentForm.DesktopLocation.Y);
+                    snapped = true;
+                }
+                else
+                    snapped = false;
+                doingStuff = false;
+            }
         }
 
         public LiveCodeEditor()
