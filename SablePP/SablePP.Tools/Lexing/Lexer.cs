5f99c8ac399c4b3dfefbd1046a182afa7aa51bdc
deaddog <mikkelslsl@gmail.com>
2014-06-26 12:35:04 +0200

    Copied the SableCC PushbackReader to the new generic Lexer.

--- SablePP/SablePP.Tools/Lexing/Lexer.cs
+++ SablePP/SablePP.Tools/Lexing/Lexer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +8,35 @@ namespace SablePP.Tools.Lexing
 {
     public abstract class Lexer : ILexer
     {
+        private class PushbackReader
+        {
+            private TextReader reader;
+            private Stack<int> stack;
+
+            public PushbackReader(TextReader reader)
+            {
+                this.reader = reader;
+                this.stack = new Stack<int>();
+            }
+
+            public int Peek()
+            {
+                if (stack.Count > 0)
+                    return stack.Peek();
+                return reader.Peek();
+            }
+
+            public int Read()
+            {
+                if (stack.Count > 0)
+                    return stack.Pop();
+                return reader.Read();
+            }
+
+            public void Unread(int v)
+            {
+                stack.Push(v);
+            }
+        }
     }
 }
