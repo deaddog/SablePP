11fde98cb961e6cab1f968ea1316a6ab2739396c
deaddog <mikkelslsl@gmail.com>
2014-06-26 12:22:16 +0200

    Created an abstract Lexer class.

---
+++ SablePP/SablePP.Tools/Lexing/Lexer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Tools.Lexing
+{
+    public abstract class Lexer : ILexer
+    {
+    }
+}
