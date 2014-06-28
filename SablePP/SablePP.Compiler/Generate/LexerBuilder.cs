3bbb81590ba45c7ff35bd626d6d4833fda3e5d3e
deaddog <mikkelslsl@gmail.com>
2014-06-28 15:26:06 +0200

    Created a new class for building the lexer using the new scheme.

---
+++ SablePP/SablePP.Compiler/Generate/LexerBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Compiler.Generate
+{
+    public class LexerBuilder : GenerateVisitor
+    {
+    }
+}
