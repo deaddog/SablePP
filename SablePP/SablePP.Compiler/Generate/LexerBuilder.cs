ca21b0f90bd6519ff29d9e52eff053bced8862df
deaddog <mikkelslsl@gmail.com>
2014-06-28 21:10:05 +0200

    Copied the file generation scheme for the lexer.

--- SablePP/SablePP.Compiler/Generate/LexerBuilder.cs
+++ SablePP/SablePP.Compiler/Generate/LexerBuilder.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using SablePP.Compiler.Nodes;
+using SablePP.Tools.Generate.CSharp;
+using SablePP.Tools.Nodes;
+using System.IO;
 
 namespace SablePP.Compiler.Generate
 {
     public class LexerBuilder : GenerateVisitor
     {
+        private FileElement fileElement;
+
+        private LexerBuilder()
+        {
+            fileElement = new FileElement();
+            fileElement.Using.Add("System");
+        }
+
+        public static FileElement BuildCode(string originalFile, Start<PGrammar> astRoot)
+        {
+            string code;
+            using (StreamReader reader = new StreamReader(originalFile))
+                code = reader.ReadToEnd();
+
+            LexerBuilder n = new LexerBuilder();
+            n.Visit(astRoot);
+
+            return n.fileElement;
+        }
     }
 }
