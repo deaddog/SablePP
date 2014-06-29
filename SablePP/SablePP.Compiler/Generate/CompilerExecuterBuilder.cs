1dac10f6e70fb3fa026acb33bd5fc3d7d3cc3a36
deaddog <mikkelslsl@gmail.com>
2014-06-29 17:18:44 +0200

    Reduced the list of using statements generated by the CompilerExecuterBuilder and declared some Tools members using their full name.

--- SablePP/SablePP.Compiler/Generate/CompilerExecuterBuilder.cs
+++ SablePP/SablePP.Compiler/Generate/CompilerExecuterBuilder.cs
@@ -18,17 +18,12 @@ namespace SablePP.Compiler.Generate
             string rootProduction = root.Root.RootProduction;
 
             FileElement fileElement = new FileElement();
-            fileElement.Using.Add("System");
             fileElement.Using.Add("System.Drawing");
             fileElement.Using.Add("System.IO");
 
             fileElement.Using.EmitNewline();
 
-            fileElement.Using.Add(ToolsNamespace.Root);
-            fileElement.Using.Add(ToolsNamespace.Error);
-            fileElement.Using.Add(ToolsNamespace.Lexing);
             fileElement.Using.Add(ToolsNamespace.Nodes);
-            fileElement.Using.Add(ToolsNamespace.Parsing);
 
             fileElement.Using.EmitNewline();
 
@@ -67,7 +62,7 @@ namespace SablePP.Compiler.Generate
             NameSpaceElement name = new NameSpaceElement(packageName);
             fileElement.Add(name);
 
-            ClassElement executerClass = new ClassElement("public partial class CompilerExecuter : CompilerExecuter<" + rootProduction + ", Lexer, Parser>");
+            ClassElement executerClass = new ClassElement("public partial class CompilerExecuter : {1}.CompilerExecuter<{0}, Lexer, Parser>", rootProduction, ToolsNamespace.Root);
             name.Add(executerClass);
 
             return executerClass;
@@ -82,7 +77,7 @@ namespace SablePP.Compiler.Generate
         }
         private static void CreateParserMethod(ClassElement classElement)
         {
-            MethodElement imMethod = new MethodElement("public override Parser GetParser(ILexer lexer)");
+            MethodElement imMethod = new MethodElement("public override Parser GetParser(" + ToolsNamespace.Lexing + ".ILexer lexer)");
             imMethod.Body.EmitLine("return new Parser(lexer);");
 
             classElement.Add(imMethod);
