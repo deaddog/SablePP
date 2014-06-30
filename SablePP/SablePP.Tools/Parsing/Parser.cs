e1e206f9c06145ca6b60d05ce19cd7a44c3b94b1
deaddog <mikkelslsl@gmail.com>
2014-06-30 09:55:18 +0200

    Created a stack variable in the generic Parser and provided the shared Push and Pop methods and the State property.

--- SablePP/SablePP.Tools/Parsing/Parser.cs
+++ SablePP/SablePP.Tools/Parsing/Parser.cs
@@ -1,8 +1,34 @@
 ﻿using SablePP.Tools.Nodes;
+using System;
+using System.Collections.Generic;
 
 namespace SablePP.Tools.Parsing
 {
     public abstract class Parser<TRoot> : IParser where TRoot : Production
     {
+        #region Stack
+
+        private Stack<Tuple<int, object>> stack;
+
+        public N Pop<N>() where N : class
+        {
+            var obj = stack.Pop();
+            return obj.Item2 as N;
+        }
+        public void Push<N>(int state, N obj)
+        {
+            stack.Push(Tuple.Create(state, (object)obj));
+        }
+        public int State
+        {
+            get { return stack.Peek().Item1; }
+        }
+
+        #endregion
+
+        public Parser()
+        {
+            this.stack = new Stack<Tuple<int, object>>();
+        }
     }
 }
