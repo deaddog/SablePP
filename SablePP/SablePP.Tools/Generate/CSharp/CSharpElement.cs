00afb13be2e679a7153bb59e9c0ee89a69bcc043
deaddog <mikkelslsl@gmail.com>
2014-06-30 11:44:24 +0200

    CSharpElement implements the ContentElement class and exposes region methods (that allow for newline spacing).

--- SablePP/SablePP.Tools/Generate/CSharp/CSharpElement.cs
+++ SablePP/SablePP.Tools/Generate/CSharp/CSharpElement.cs
@@ -3,22 +3,37 @@
     /// <summary>
     /// Provides an extension to the <see cref="ComplexElement"/> specific to C#.
     /// </summary>
-    public abstract class CSharpElement : ComplexElement
+    public abstract class CSharpElement<Content> : ContentElement<Content> where Content : PatchElement
     {
+        public CSharpElement(Content content)
+            :base(content)
+        {
+        }
+
         /// <summary>
         /// Emits the start of a region.
         /// </summary>
         /// <param name="text">The message displayed in the first line of the region.</param>
-        public void EmitRegionStart(string text)
+        /// <param name="addSpacers">if set to <c>true</c> a blank line is inserted before and after the start of the region.</param>
+        public void EmitRegionStart(string text, bool addSpacers = true)
         {
-            base.emitLine("#region {0}", text);
+            if (addSpacers)
+                content.EmitNewLine();
+            content.EmitLine("#region {0}", text);
+            if (addSpacers)
+                content.EmitNewLine();
         }
         /// <summary>
         /// Emits the end of a region.
         /// </summary>
-        public void EmitRegionEnd()
+        /// <param name="addSpacers">if set to <c>true</c> a blank line is inserted before and after the end of the region.</param>
+        public void EmitRegionEnd(bool addSpacers = true)
         {
-            base.emitLine("#endregion");
+            if (addSpacers)
+                content.EmitNewLine();
+            content.EmitLine("#endregion");
+            if (addSpacers)
+                content.EmitNewLine();
         }
     }
 }
