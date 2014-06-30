c904f11b3a9b01f9aafb0be0770a0891d73ddaec
deaddog <mikkelslsl@gmail.com>
2014-06-30 14:29:09 +0200

    Created a class for generating the token index methods in parsers.

---
+++ SablePP/SablePP.Compiler/Generate/Parsing/TokenIndexBuilder.cs
@@ -0,0 +1,14 @@
+﻿using SablePP.Tools.Generate.CSharp;
+
+namespace SablePP.Compiler.Generate.Parsing
+{
+    public class TokenIndexBuilder : GenerateVisitor
+    {
+        private ClassElement classElement;
+
+        public TokenIndexBuilder(ClassElement classElement)
+        {
+            this.classElement = classElement;
+        }
+    }
+}
