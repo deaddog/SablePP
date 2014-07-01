cdd8bbf1b8d55ba33bad6329ca7ea1d4597ea841
deaddog <mikkelslsl@gmail.com>
2014-07-01 17:01:22 +0200

    Created a class that encapsules information about an element in a production.

---
+++ SablePP/SablePP.Compiler/Generate/Productions/ProductionElement.cs
@@ -0,0 +1,42 @@
+﻿using SablePP.Compiler.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Compiler.Generate.Productions
+{
+    public class ProductionElement
+    {
+        private PElement element;
+
+        public ProductionElement(PElement element)
+        {
+            this.element = element;
+        }
+
+        public PElement Element
+        {
+            get { return element; }
+        }
+
+        public string FieldName
+        {
+            get { return "_" + element.LowerName + "_"; }
+        }
+        public string PropertyName
+        {
+            get { return SablePP.Compiler.CommonMethods.ToCamelCase(element.LowerName); }
+        }
+
+        public ElementTypes ElementType
+        {
+            get { return element.GetElementType(); }
+        }
+
+        public override string ToString()
+        {
+            return element.ToString();
+        }
+    }
+}
