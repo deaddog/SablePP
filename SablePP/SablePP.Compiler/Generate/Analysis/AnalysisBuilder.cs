ed7ed99fd769d9a58f3b69d8ffbd8df854e63ea9
deaddog <mikkelslsl@gmail.com>
2014-07-03 11:54:43 +0200

    The number of type-arguments in the generated return adapters in managed by a constant in AnalysisBuilder.

--- SablePP/SablePP.Compiler/Generate/Analysis/AnalysisBuilder.cs
+++ SablePP/SablePP.Compiler/Generate/Analysis/AnalysisBuilder.cs
@@ -10,6 +10,8 @@ namespace SablePP.Compiler.Generate.Analysis
 {
     public class AnalysisBuilder : GenerateVisitor
     {
+        private const byte TYPEARGSCOUNT_RETURN = 3;
+
         private FileElement fileElement;
         private NameSpaceElement nameElement;
 
@@ -53,10 +55,8 @@ namespace SablePP.Compiler.Generate.Analysis
             nameElement.EmitRegionStart("Return analysis adapters", false);
             nameElement.EmitNewLine();
 
-            adapters.Add(new ReturnAnalysisAdapterBuilder(nameElement, 0, node));
-            adapters.Add(new ReturnAnalysisAdapterBuilder(nameElement, 1, node));
-            adapters.Add(new ReturnAnalysisAdapterBuilder(nameElement, 2, node));
-            adapters.Add(new ReturnAnalysisAdapterBuilder(nameElement, 3, node));
+            for (byte i = 0; i <= TYPEARGSCOUNT_RETURN; i++)
+                adapters.Add(new ReturnAnalysisAdapterBuilder(nameElement, i, node));
 
             nameElement.EmitNewLine();
             nameElement.EmitRegionEnd(false);
