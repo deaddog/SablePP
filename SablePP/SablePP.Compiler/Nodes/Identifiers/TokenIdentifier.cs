eb705ae072f4b7a9a0af160ce5fef28bef89bf74
deaddog <mikkelslsl@gmail.com>
2014-07-10 14:06:31 +0200

    Created a separate identifier type for identifiers referring a Token.

---
+++ SablePP/SablePP.Compiler/Nodes/Identifiers/TokenIdentifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Compiler.Nodes.Identifiers
+{
+    public class TokenIdentifier : TIdentifier
+    {
+        private AToken token;
+
+        public static TokenIdentifier Replace(TIdentifier identifier, AToken token)
+        {
+            TokenIdentifier t = new TokenIdentifier(identifier.Text, identifier.Line, identifier.Position, token);
+            identifier.ReplaceBy(t);
+            return t;
+        }
+
+        private TokenIdentifier(string text, int line, int pos, AToken token)
+            :base(text, line, pos)
+        {
+            this.token = token;
+        }
+
+        public AToken Declaration
+        {
+            get { return token; }
+        }
+    }
+}
