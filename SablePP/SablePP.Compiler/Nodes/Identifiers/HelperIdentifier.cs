eac120ed1cd3be4e7f3344da60d66acd271dd391
deaddog <mikkelslsl@gmail.com>
2014-07-14 15:08:32 +0200

    Created a separate identifier type for identifiers referring a Helper.

---
+++ SablePP/SablePP.Compiler/Nodes/Identifiers/HelperIdentifier.cs
@@ -0,0 +1,18 @@
+﻿namespace SablePP.Compiler.Nodes.Identifiers
+{
+    public class HelperIdentifier : TIdentifier
+    {
+        private PHelper helper;
+
+        public HelperIdentifier(TIdentifier identifier, PHelper helper)
+            :base(identifier)
+        {
+            this.helper = helper;
+        }
+
+        public PHelper Declaration
+        {
+            get { return helper; }
+        }
+    }
+}
