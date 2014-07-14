6d002e3358f1beb09565c1980c97fdb3301c7c91
deaddog <mikkelslsl@gmail.com>
2014-07-14 15:31:05 +0200

    Created an identifier type for states. This points to an identifier as declaration, as states are not represented as productions in the ast.

---
+++ SablePP/SablePP.Compiler/Nodes/Identifiers/StateIdentifier.cs
@@ -0,0 +1,26 @@
+﻿namespace SablePP.Compiler.Nodes.Identifiers
+{
+    public class StateIdentifier : TIdentifier
+    {
+        private StateIdentifier declaration;
+
+        public StateIdentifier(TIdentifier identifier, StateIdentifier declaration)
+            : base(identifier.Text, identifier.Line, identifier.Position)
+        {
+            if (declaration == null)
+                this.declaration = this;
+            else
+                this.declaration = declaration;
+        }
+
+        public bool IsDeclaration
+        {
+            get { return this.declaration == this; }
+        }
+
+        public StateIdentifier Declaration
+        {
+            get { return declaration; }
+        }
+    }
+}
