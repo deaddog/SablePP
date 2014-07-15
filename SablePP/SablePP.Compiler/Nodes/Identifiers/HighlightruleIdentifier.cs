09fdc6a2245a8bc2da22d83bf79e7ab8ac8a0828
deaddog <mikkelslsl@gmail.com>
2014-07-15 18:27:08 +0200

    Fixed typo; HightlightruleIdentifier > HighlightruleIdentifier.

---
+++ SablePP/SablePP.Compiler/Nodes/Identifiers/HighlightruleIdentifier.cs
@@ -0,0 +1,10 @@
+﻿namespace SablePP.Compiler.Nodes.Identifiers
+{
+    public class HighlightruleIdentifier : DeclarationIdentifier<PHighlightrule>
+    {
+        public HighlightruleIdentifier(TIdentifier identifier, PHighlightrule rule)
+            : base(identifier, rule)
+        {
+        }
+    }
+}
--- SablePP/SablePP.Compiler/Validation/SymbolLinking/HighlightrulesTable.cs
+++ SablePP/SablePP.Compiler/Validation/SymbolLinking/HighlightrulesTable.cs
@@ -3,11 +3,11 @@ using SablePP.Compiler.Nodes.Identifiers;
 
 namespace SablePP.Compiler.Validation.SymbolLinking
 {
-    public class HighlightrulesTable : DeclarationTable<HightlightruleIdentifier, PHighlightrule>
+    public class HighlightrulesTable : DeclarationTable<HighlightruleIdentifier, PHighlightrule>
     {
-        protected override HightlightruleIdentifier construct(TIdentifier identifier, PHighlightrule declaration)
+        protected override HighlightruleIdentifier construct(TIdentifier identifier, PHighlightrule declaration)
         {
-            return new HightlightruleIdentifier(identifier, declaration);
+            return new HighlightruleIdentifier(identifier, declaration);
         }
 
         protected override TIdentifier getIdentifier(PHighlightrule declaration)
