9068d0fcc026a3779f64c8a5dd8360aefb0fe2e2
deaddog <mikkelslsl@gmail.com>
2014-07-15 11:18:25 +0200

    Created a partial definition for PToken declaring a IsIgnored property.

---
+++ SablePP/SablePP.Compiler/Nodes/PToken.cs
@@ -0,0 +1,12 @@
+﻿namespace SablePP.Compiler.Nodes
+{
+    public partial class PToken
+    {
+        private bool? ignored = null;
+        public bool IsIgnored
+        {
+            get { return ignored.HasValue ? ignored.Value : false; }
+            set { ignored = value; }
+        }
+    }
+}
