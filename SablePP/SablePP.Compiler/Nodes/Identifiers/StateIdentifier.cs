3b15ac361d932b858b95648cc3bb62192931c47a
deaddog <mikkelslsl@gmail.com>
2014-07-15 10:36:15 +0200

    Created a specialized table for handling State declarations.

--- SablePP/SablePP.Compiler/Nodes/Identifiers/StateIdentifier.cs
+++ SablePP/SablePP.Compiler/Nodes/Identifiers/StateIdentifier.cs
@@ -3,14 +3,20 @@
     public class StateIdentifier : TIdentifier
     {
         private StateIdentifier declaration;
+        private bool first;
+
+        public StateIdentifier(TIdentifier identifier, bool first)
+            : base(identifier.Text, identifier.Line, identifier.Position)
+        {
+            this.declaration = this;
+            this.first = first;
+        }
 
         public StateIdentifier(TIdentifier identifier, StateIdentifier declaration)
             : base(identifier.Text, identifier.Line, identifier.Position)
         {
-            if (declaration == null)
-                this.declaration = this;
-            else
-                this.declaration = declaration;
+            this.declaration = declaration;
+            this.first = declaration.first;
         }
 
         public bool IsDeclaration
@@ -21,6 +27,11 @@
         public StateIdentifier Declaration
         {
             get { return declaration; }
+        }
+
+        public bool IsFirst
+        {
+            get { return first; }
         }
     }
 }
