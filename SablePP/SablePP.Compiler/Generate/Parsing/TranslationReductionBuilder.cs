fe65957df1ff699fb79dff6b8071f871515e74eb
deaddog <mikkelslsl@gmail.com>
2014-07-16 13:39:54 +0200

    TranslationReductionBuilder generates Pop and Push code for each alternative.

--- SablePP/SablePP.Compiler/Generate/Parsing/TranslationReductionBuilder.cs
+++ SablePP/SablePP.Compiler/Generate/Parsing/TranslationReductionBuilder.cs
@@ -2,22 +2,59 @@
 using SablePP.Compiler.Nodes;
 using SablePP.Tools.Generate;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SablePP.Compiler.Generate.Parsing
 {
     public class TranslationReductionBuilder : ReductionBuilder
     {
-        private readonly Dictionary<PTranslation, string> variableNames;
+        private readonly Dictionary<PTranslation, string> translationVariables;
+        private readonly Dictionary<PElement, string> elementVariables;
 
         public TranslationReductionBuilder()
         {
-            this.variableNames = new Dictionary<PTranslation, string>();
+            this.translationVariables = new Dictionary<PTranslation, string>();
+            this.elementVariables = new Dictionary<PElement, string>();
         }
 
         public override void CaseAAlternative(Nodes.AAlternative node)
         {
             CreateNewCase();
+
+            var collection = node.Elements.Element;
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                var element = collection[i];
+                string className = element.ClassName;
+                elementVariables[element] = GetVariable(className);
+
+                PProduction production = element.Elementid.Identifier.IsProduction ? element.Elementid.Identifier.AsProduction.Declaration : null;
+
+                if (node.HasTranslation && (production == null || hasAstProduction(production)))
+                    code.EmitLine("{0} {1} = Pop<{0}>();", className, elementVariables[element]);
+                else
+                    code.EmitLine("Pop<object>();");
+            }
+
             base.CaseAAlternative(node);
+
+            if (node.HasTranslation)
+                code.EmitLine("Push({0}, {1});", GoTo, translationVariables[node.Translation]);
+            else
+                code.EmitLine("Push({0}, new object());", GoTo);
+        }
+
+        private bool hasAstProduction(PProduction production)
+        {
+            if (production.HasProdtranslation)
+                return true;
+
+            PGrammar grammar = production.GetFirstParent<PGrammar>();
+
+            if (!grammar.HasAstproductions)
+                return false;
+
+            return grammar.Astproductions.Productions.Where(p => p.ClassName == production.ClassName).Any();
         }
     }
 }
