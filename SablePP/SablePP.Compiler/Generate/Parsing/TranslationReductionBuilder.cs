45cfb201d750d0d7c296de6f4e0dc758c0bee5cc
deaddog <mikkelslsl@gmail.com>
2014-07-16 12:29:25 +0200

    TranslationReductionBuilder defines a dictionary for variable names based on PTranslation elements.

--- SablePP/SablePP.Compiler/Generate/Parsing/TranslationReductionBuilder.cs
+++ SablePP/SablePP.Compiler/Generate/Parsing/TranslationReductionBuilder.cs
@@ -1,9 +1,23 @@
 ﻿using SablePP.Compiler.Analysis;
+using SablePP.Compiler.Nodes;
 using SablePP.Tools.Generate;
+using System.Collections.Generic;
 
 namespace SablePP.Compiler.Generate.Parsing
 {
     public class TranslationReductionBuilder : ReductionBuilder
     {
+        private readonly Dictionary<PTranslation, string> variableNames;
+
+        public TranslationReductionBuilder()
+        {
+            this.variableNames = new Dictionary<PTranslation, string>();
+        }
+
+        public override void CaseAAlternative(Nodes.AAlternative node)
+        {
+            CreateNewCase();
+            base.CaseAAlternative(node);
+        }
     }
 }
