96a07dd0af868653f104c231292df5bda516d8b4
deaddog <mikkelslsl@gmail.com>
2014-07-22 08:35:59 +0200

    Added a IsFirst property to PProduction, replacing the one that used to be in ProductionIdentifier.

--- SablePP/SablePP.Compiler/Nodes/PProduction.cs
+++ SablePP/SablePP.Compiler/Nodes/PProduction.cs
@@ -2,6 +2,19 @@
 {
     public partial class PProduction
     {
+        public bool IsFirst
+        {
+            get
+            {
+                var par = this.GetParent();
+                if (par is PProductions)
+                    return ReferenceEquals((par as PProductions).Productions[0], this);
+                else if (par is PAstproductions)
+                    return ReferenceEquals((par as PAstproductions).Productions[0], this);
+                else
+                    return false;
+            }
+        }
         public string ClassName
         {
             get { return "P" + this.Identifier.Text.ToCamelCase(); }
