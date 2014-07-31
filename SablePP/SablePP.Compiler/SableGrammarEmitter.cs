2a8de4891590573e2bf394a87e3a6c3247932031
deaddog <mikkelslsl@gmail.com>
2014-07-31 14:10:08 +0200

    SableGrammarEmitter Adds newlines close to the code-style used for the sablepp grammar.

--- SablePP/SablePP.Compiler/SableGrammarEmitter.cs
+++ SablePP/SablePP.Compiler/SableGrammarEmitter.cs
@@ -40,6 +40,10 @@ namespace SablePP.Compiler
             byte[] buffer = encoding.GetBytes(text);
             stream.Write(buffer, 0, buffer.Length);
         }
+        private void writeNewline()
+        {
+            write("\r\n");
+        }
 
         public override void CaseTPackagetoken(Nodes.TPackagetoken node)
         {
@@ -52,7 +56,64 @@ namespace SablePP.Compiler
                 write((node as Token).Text  +  " ");
             else
                 base.DefaultCase(node);
-            }
+        }
+
+        // Newline rules
+
+        public override void CaseAPackage(Nodes.APackage node)
+        {
+            base.CaseAPackage(node);
+            writeNewline();
+        }
+
+        public override void CaseTHelperstoken(Nodes.THelperstoken node)
+        {
+            base.CaseTHelperstoken(node);
+            writeNewline();
+        }
+        public override void CaseAHelper(Nodes.AHelper node)
+        {
+            base.CaseAHelper(node);
+            writeNewline();
+        }
+
+        public override void CaseAStates(Nodes.AStates node)
+        {
+            base.CaseAStates(node);
+            writeNewline();
+        }
+
+        public override void CaseTTokenstoken(Nodes.TTokenstoken node)
+        {
+            base.CaseTTokenstoken(node);
+            writeNewline();
+        }
+        public override void CaseAToken(Nodes.AToken node)
+        {
+            base.CaseAToken(node);
+            writeNewline();
+        }
+
+        public override void CaseAIgnoredtokens(Nodes.AIgnoredtokens node)
+        {
+            base.CaseAIgnoredtokens(node);
+            writeNewline();
+        }
+
+        public override void CaseTProductionstoken(Nodes.TProductionstoken node)
+        {
+            base.CaseTProductionstoken(node);
+            writeNewline();
+        }
+        public override void CaseTAsttoken(Nodes.TAsttoken node)
+        {
+            base.CaseTAsttoken(node);
+            writeNewline();
+        }
+        public override void CaseAProduction(Nodes.AProduction node)
+        {
+            base.CaseAProduction(node);
+            writeNewline();
         }
     }
 }
