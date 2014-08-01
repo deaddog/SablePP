e52e710335ab98f09aa93d0898d5161b35c37a03
deaddog <mikkelslsl@gmail.com>
2014-08-01 17:24:11 +0200

    Logger writes a simple log file to executionDir/log/.

--- SablePP/SablePP.Compiler/SableCCLogger.cs
+++ SablePP/SablePP.Compiler/SableCCLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,7 +10,27 @@ namespace SablePP.Compiler
     {
         public static void LogFromGrammar(string inputGrammar, string modifiedGrammar, string errorString)
         {
-            throw new NotImplementedException();
+            DateTime time = DateTime.Now;
+
+            string logDir = Path.Combine(PathInformation.ExecutingDirectory, "log");
+            if (!Directory.Exists(logDir))
+                Directory.CreateDirectory(logDir);
+
+            string logPath = Path.Combine(logDir, time.ToString("yyyyMMdd.HHmmss.fff") + ".txt");
+            using (StreamWriter writer = new StreamWriter(logPath))
+            {
+                string s = time.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                writer.WriteLine("### Date/Time ###");
+                writer.WriteLine("Date: {0:yyyy-MM-dd}");
+                writer.WriteLine("Time: {0:HH:mm:ss.fff}", time);
+
+                writer.WriteLine("### SablePP grammar ###");
+                writer.WriteLine(inputGrammar);
+                writer.WriteLine("### SableCC grammar ###");
+                writer.WriteLine(modifiedGrammar);
+                writer.WriteLine("### SableCC error message ###");
+                writer.WriteLine(errorString);
+            }
         }
     }
 }
