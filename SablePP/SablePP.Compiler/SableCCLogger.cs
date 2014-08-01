29ca9ca07518fcda1e5525800dc7ca4094c1d490
deaddog <mikkelslsl@gmail.com>
2014-08-01 14:58:39 +0200

    Created a class for storing error logs from SableCC.

---
+++ SablePP/SablePP.Compiler/SableCCLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Compiler
+{
+    public static class SableCCLogger
+    {
+        public static void LogFromGrammar(string inputGrammar, string modifiedGrammar, string errorString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
