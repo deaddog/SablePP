a6002105bd655ba1b4cd5f0d8d17a08c7c8d0374
deaddog <mikkelslsl@gmail.com>
2014-08-02 14:43:01 +0200

    Production translations from 'name' to 'name' are not required by sable.

--- SablePP/SablePP.Compiler/Validation/ExcessiveNodesVisitor.cs
+++ SablePP/SablePP.Compiler/Validation/ExcessiveNodesVisitor.cs
@@ -9,5 +9,16 @@ namespace SablePP.Compiler.Validation
             : base(errorManager)
         {
         }
+
+        public override void InPProdtranslation(PProdtranslation node)
+        {
+            var prod = node.GetFirstParent<PProduction>();
+            var prodA = node.Identifier.AsPProduction;
+
+            if (prod != null && prodA != null && prod.ClassName == prodA.ClassName)
+                RegisterMessage(node, "It is not required to add a production translation for \"{0} -> {0}\".", node.Identifier.Text);
+
+            base.InPProdtranslation(node);
+        }
     }
 }
