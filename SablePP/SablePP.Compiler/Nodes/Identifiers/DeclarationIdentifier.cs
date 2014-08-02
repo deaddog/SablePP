d48be7139385bf817aacffd3be9a4fd1fa83d0f0
deaddog <mikkelslsl@gmail.com>
2014-08-03 01:06:29 +0200

    Creates a non-generic DeclarationIdentifier as an intermediate between TIdentifier and the generic DeclarationIdentifier.

--- SablePP/SablePP.Compiler/Nodes/Identifiers/DeclarationIdentifier.cs
+++ SablePP/SablePP.Compiler/Nodes/Identifiers/DeclarationIdentifier.cs
@@ -2,17 +2,30 @@
 
 namespace SablePP.Compiler.Nodes.Identifiers
 {
-    public class DeclarationIdentifier<T> : TIdentifier where T : Production
+    public class DeclarationIdentifier<T> : DeclarationIdentifier where T : Production
     {
-        private T declaration;
-
         public DeclarationIdentifier(TIdentifier identifier, T declaration)
-            : base(identifier.Text, identifier.Line, identifier.Position)
+            : base(identifier.Text, identifier.Line, identifier.Position, declaration)
+        {
+        }
+
+        new public T Declaration
+        {
+            get { return base.Declaration as T; }
+        }
+    }
+
+    public class DeclarationIdentifier : TIdentifier
+    {
+        private Node declaration;
+
+        public DeclarationIdentifier(string text, int line, int position, Node declaration)
+            :base(text, line, position)
         {
             this.declaration = declaration;
         }
 
-        public T Declaration
+        public Node Declaration
         {
             get { return declaration; }
         }
