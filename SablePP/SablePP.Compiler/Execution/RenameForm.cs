806e0cfd977db88e6ccdb7130deb89c7475ac2d7
deaddog <mikkelslsl@gmail.com>
2014-08-07 13:04:34 +0200

    Adds a method to the RenameForm that determines (as a string) what type of identifier is being renamed.

--- SablePP/SablePP.Compiler/Execution/RenameForm.cs
+++ SablePP/SablePP.Compiler/Execution/RenameForm.cs
@@ -1,16 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using SablePP.Compiler.Nodes;
+using System;
 using System.Windows.Forms;
 
 namespace SablePP.Compiler.Execution
 {
     public partial class RenameForm : Form
     {
+        public static string GetDeclarationType(DeclarationIdentifier identifier)
+        {
+            if (identifier.IsPAlternative)
+                return "alternative";
+            else if (identifier.IsPElement)
+                return "production element";
+            else if (identifier.IsPHelper)
+                return "helper";
+            else if (identifier.IsPProduction)
+            {
+                var ast = identifier.AsPProduction.GetFirstParent<AASTSection>();
+                if (ast == null)
+                    return "production";
+                else
+                    return "ast production";
+            }
+            else if (identifier.IsPToken)
+                return "token";
+            else if (identifier.IsState)
+                return "state";
+            else
+                return null;
+        }
+
         public RenameForm()
         {
             InitializeComponent();
