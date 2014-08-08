b2bb763da698e205354adc350f843428de16c7e0
deaddog <mikkelslsl@gmail.com>
2014-08-08 14:13:33 +0200

    Created a partial definition for PTranslation, defining the properties required by the TranslationReductionBuilder.

---
+++ SablePP/SablePP.Compiler/Nodes/PTranslation.cs
@@ -0,0 +1,15 @@
+﻿namespace SablePP.Compiler.Nodes
+{
+    public partial class PTranslation
+    {
+        public abstract string ClassName
+        {
+            get;
+        }
+
+        public abstract bool IsList
+        {
+            get;
+        }
+    }
+}
