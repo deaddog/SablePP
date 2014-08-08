0a49f8e83396df97480f672029dcb5e5ef32fc20
deaddog <mikkelslsl@gmail.com>
2014-08-08 14:36:37 +0200

    Adds partial definitions for all translation productions, implementing abstract properties similarly to the methods defined in the TranslationReductionBuilder.

---
+++ SablePP/SablePP.Compiler/Nodes/AFullTranslation.cs
@@ -0,0 +1,15 @@
+﻿namespace SablePP.Compiler.Nodes
+{
+    public partial class AFullTranslation
+    {
+        public override string ClassName
+        {
+            get { return Translation.ClassName; }
+        }
+
+        public override bool IsList
+        {
+            get { return Translation.IsList; }
+        }
+    }
+}
---
+++ SablePP/SablePP.Compiler/Nodes/AListTranslation.cs
@@ -0,0 +1,15 @@
+﻿namespace SablePP.Compiler.Nodes
+{
+   public partial class AListTranslation
+    {
+        public override string ClassName
+        {
+            get { return Elements[0].ClassName; }
+        }
+
+        public override bool IsList
+        {
+            get { return true; }
+        }
+    }
+}
