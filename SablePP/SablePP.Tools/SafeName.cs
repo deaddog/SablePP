b49a0a5cfbf7e996c1f29d84f59d0d7f68f1adde
deaddog <mikkelslsl@gmail.com>
2014-08-13 21:53:51 +0200

    SafeName can optionally try to use the input string as the end-name when running GetName.

--- SablePP/SablePP.Tools/SafeName.cs
+++ SablePP/SablePP.Tools/SafeName.cs
@@ -14,15 +14,27 @@ namespace SablePP.Tools
         private Func<string, IEnumerable<string>> testnames;
 
         /// <summary>
-        /// Enumerates a collection of strings matching the pattern '<paramref name="name"/>', '<paramref name="name"/>1', '<paramref name="name"/>2', ...
+        /// Enumerates a collection of strings matching the pattern '<paramref name="name"/>1', '<paramref name="name"/>2', '<paramref name="name"/>3', ...
         /// </summary>
         /// <param name="name">The name that should be prepended with integers.</param>
-        /// <returns>An infinite collection of strings; '<paramref name="name"/>', '<paramref name="name"/>1', '<paramref name="name"/>2', ...</returns>
+        /// <returns>An infinite collection of strings; '<paramref name="name"/>1', '<paramref name="name"/>2', '<paramref name="name"/>3', ...</returns>
         public static IEnumerable<string> GetNumberedNames(string name)
         {
-            yield return name;
             int i = 1;
             while (true) yield return name + i++;
+        }
+
+        private bool yieldInitial = false;
+        /// <summary>
+        /// Gets or sets a value indicating whether the input name should always be tested as an available name.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the input string should be tested; otherwise, <c>false</c>.
+        /// </value>
+        public bool YieldInitial
+        {
+            get { return yieldInitial; }
+            set { yieldInitial = value; }
         }
 
         /// <summary>
@@ -72,6 +84,15 @@ namespace SablePP.Tools
         /// <returns>A string not previously known by this <see cref="SafeName"/>.</returns>
         public string GetName(string name)
         {
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            if(!names.Contains(name))
+            {
+                names.Add(name);
+                return name;
+            }
+
             foreach (var n in testnames(name))
                 if (!names.Contains(n))
                 {
