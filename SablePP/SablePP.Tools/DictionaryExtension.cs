fa78ed323387835b134573ea88bf4ae28c57fd2b
deaddog <mikkelslsl@gmail.com>
2014-08-13 09:40:40 +0200

    Adds a maxDistance argument to the GetNearest methods.

--- SablePP/SablePP.Tools/DictionaryExtension.cs
+++ SablePP/SablePP.Tools/DictionaryExtension.cs
@@ -15,9 +15,10 @@ namespace SablePP.Tools
         /// <typeparam name="T">The type of the values contained by the dictionary.</typeparam>
         /// <param name="dict">The dictionary form which a value is extracted.</param>
         /// <param name="key">The key to look for in the dictionary.</param>
+        /// <param name="maxDistance">The maximum "distance" allowed when comparing keys.</param>
         /// <returns>The value associated with the key that has the closest edit distance to <paramref name="key"/>.
         /// If the dictionary is empty; <c>default(T)</c>.</returns>
-        public static T GetNearest<T>(this IDictionary<string, T> dict, string key)
+        public static T GetNearest<T>(this IDictionary<string, T> dict, string key, int maxDistance = int.MaxValue)
         {
             if (key == null)
                 throw new ArgumentNullException("key");
@@ -37,7 +38,10 @@ namespace SablePP.Tools
                 }
             }
 
-            return t;
+            if (diff > maxDistance)
+                return default(T);
+            else
+                return t;
         }
 
         /// <summary>
@@ -48,9 +52,10 @@ namespace SablePP.Tools
         /// <param name="collection">The collection from which a value is extracted.</param>
         /// <param name="key">The key to look for in the collection.</param>
         /// <param name="translate">A method that translates an object of type <typeparamref name="T"/> into a key string..</param>
+        /// <param name="maxDistance">The maximum "distance" allowed when comparing keys.</param>
         /// <returns>The value associated with the key (translated from objects) that has the closest edit distance to <paramref name="key"/>.
         /// If the collection is empty; <c>default(T)</c>.</returns>
-        public static T GetNearest<T>(this IEnumerable<T> collection, string key, Func<T, string> translate)
+        public static T GetNearest<T>(this IEnumerable<T> collection, string key, Func<T, string> translate, int maxDistance = int.MaxValue)
         {
             if (key == null)
                 throw new ArgumentNullException("key");
@@ -70,7 +75,10 @@ namespace SablePP.Tools
                 }
             }
 
-            return t;
+            if (diff > maxDistance)
+                return default(T);
+            else
+                return t;
         }
 
         private static int editDistance(string s, string t)
