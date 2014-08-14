1492fbc0d465d219478e051799d7ee8505234db0
deaddog <mikkelslsl@gmail.com>
2014-08-14 13:07:34 +0200

    Creates a declaration interface that returns the identifier of a declaration.

---
+++ SablePP/SablePP.Compiler/Nodes/IDeclaration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Compiler.Nodes
+{
+    public interface IDeclaration
+    {
+        TIdentifier GetIdentifier();
+    }
+}
