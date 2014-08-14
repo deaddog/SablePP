0dea47f76dd114c4199bb6c2d2dbb934550dbcfb
deaddog <mikkelslsl@gmail.com>
2014-08-14 11:27:39 +0200

    StatesTable uses PState instead of StateIdentifier.

--- SablePP/SablePP.Compiler/Validation/SymbolLinking/StatesTable.cs
+++ SablePP/SablePP.Compiler/Validation/SymbolLinking/StatesTable.cs
@@ -4,69 +4,16 @@ using System.Collections.Generic;
 
 namespace SablePP.Compiler.Validation.SymbolLinking
 {
-    public class StatesTable
+    public class StatesTable : DeclarationTable<PState>
     {
-        private Dictionary<string, StateIdentifier> declarations;
-        private List<StateIdentifier> unusedList;
-
-        public StatesTable()
+        protected override DeclarationIdentifier<PState> construct(TIdentifier identifier, PState declaration)
         {
-            this.declarations = new Dictionary<string, StateIdentifier>();
-            this.unusedList = new List<StateIdentifier>();
+            return new DeclarationIdentifier<PState>(identifier, declaration);
         }
 
-        public StateIdentifier this[string text]
+        protected override TIdentifier getIdentifier(PState declaration)
         {
-            get { return declarations[text]; }
-        }
-
-        public bool Declare(TIdentifier declaration)
-        {
-            string text = declaration.Text;
-
-            if (declarations.ContainsKey(text))
-                return false;
-            else
-            {
-                StateIdentifier reference = new StateIdentifier(declaration, declarations.Count == 0);
-                declaration.ReplaceBy(reference);
-                declarations.Add(text, reference);
-
-                if (!reference.IsFirst)
-                    unusedList.Add(reference);
-
-                return true;
-            }
-        }
-
-        public bool Link(TIdentifier identifier)
-        {
-            StateIdentifier declaration = null;
-            if (declarations.TryGetValue(identifier.Text, out declaration))
-            {
-                StateIdentifier reference = new StateIdentifier(identifier, declaration);
-                identifier.ReplaceBy(reference);
-                unusedList.Remove(declaration);
-
-                return true;
-            }
-            else
-                return false;
-        }
-
-        public bool Contains(string text)
-        {
-            return declarations.ContainsKey(text);
-        }
-
-        public IEnumerable<StateIdentifier> NonLinked
-        {
-            get
-            {
-                foreach (var d in unusedList)
-                    yield return d;
-            }
+            return declaration.Identifier;
         }
     }
-
 }
