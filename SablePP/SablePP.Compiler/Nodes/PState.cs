5959142b35d59d44ae66b260a51c020c49d4ead3
deaddog <mikkelslsl@gmail.com>
2014-08-14 11:18:24 +0200

    Creates a partial definition for PState, that exposes a IsFirst property.

---
+++ SablePP/SablePP.Compiler/Nodes/PState.cs
@@ -0,0 +1,14 @@
+﻿namespace SablePP.Compiler.Nodes
+{
+    public partial class PState
+    {
+        public bool IsFirst
+        {
+            get
+            {
+                var par = this.GetFirstParent<PStates>();
+                return ReferenceEquals(par.States[0], this);
+            }
+        }
+    }
+}
