77c1940fd43ed7dd202512e57914fc7f74051004
deaddog <mikkelslsl@gmail.com>
2014-08-21 17:43:56 +0200

    Adds a Helper class similar to the Token class (without states).

---
+++ SablePP/SablePP.Generate/Helper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SablePP.Generate
+{
+   public class Helper
+    {
+        private string name;
+        private RegularExpression expression;
+
+        public Helper(string name, RegularExpression expression)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            if (expression == null)
+                throw new ArgumentNullException("expression");
+
+            this.name = name;
+            this.expression = expression;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public RegularExpression Expression
+        {
+            get { return expression; }
+        }
+    }
+}
