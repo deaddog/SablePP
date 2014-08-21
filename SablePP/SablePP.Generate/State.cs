7517ed4c6b81bd2268119863b2265aa96097b891
deaddog <mikkelslsl@gmail.com>
2014-08-21 17:38:11 +0200

    Adds a State class to the SablePP.Generate project.

---
+++ SablePP/SablePP.Generate/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SablePP.Generate
+{
+    public class State
+    {
+        private readonly string name;
+
+        public State(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
