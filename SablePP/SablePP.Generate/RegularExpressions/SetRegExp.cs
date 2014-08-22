34e1b3fd5e531c7bbf31e8d2fc7e8c126d35d7dd
deaddog <mikkelslsl@gmail.com>
2014-08-22 09:54:10 +0200

    Simplifies the constructors for SetRegExp - any type of regular expression is accepted.

--- SablePP/SablePP.Generate/RegularExpressions/SetRegExp.cs
+++ SablePP/SablePP.Generate/RegularExpressions/SetRegExp.cs
@@ -7,35 +7,7 @@ namespace SablePP.Generate.RegularExpressions
         private RegExp from, to;
         private SetTypes type;
 
-        public SetRegExp(char from, char to, SetTypes setType)
-        {
-            this.from = new LiteralRegExp(from);
-            this.to = new LiteralRegExp(to);
-
-            this.type = setType;
-        }
-
-        public SetRegExp(SetRegExp from, char to, SetTypes setType)
-        {
-            if (from == null)
-                throw new ArgumentNullException("from");
-
-            this.from = from;
-            this.to = new LiteralRegExp(to);
-            this.type = setType;
-        }
-
-        public SetRegExp(char from, SetRegExp to, SetTypes setType)
-        {
-            if (to == null)
-                throw new ArgumentNullException("to");
-
-            this.from = new LiteralRegExp(from);
-            this.to = to;
-            this.type = setType;
-        }
-
-        public SetRegExp(SetRegExp from, SetRegExp to, SetTypes setType)
+        public SetRegExp(RegExp from, RegExp to, SetTypes setType)
         {
             if (from == null)
                 throw new ArgumentNullException("from");
