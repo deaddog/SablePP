4b977139cef370c5e65a38678b7c13b4d4fcd9dd
deaddog <mikkelslsl@gmail.com>
2014-08-22 09:35:46 +0200

    Implemens a specialized RegExp that handles references to helpers.

---
+++ SablePP/SablePP.Generate/ReferenceRegularExpression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SablePP.Generate
+{
+    public class ReferenceRegularExpression : RegularExpression
+    {
+        private Helper reference;
+
+        public ReferenceRegularExpression(Helper reference)
+        {
+            if (reference == null)
+                throw new ArgumentNullException("reference");
+
+            this.reference = reference;
+        }
+
+        public Helper ReferencedHelper
+        {
+            get { return reference; }
+        }
+    }
+}
