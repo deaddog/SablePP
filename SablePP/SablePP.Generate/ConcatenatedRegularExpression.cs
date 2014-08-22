dfd4a44fc567735e113e662e3d42a0796088b62d
deaddog <mikkelslsl@gmail.com>
2014-08-22 08:48:41 +0200

    Implements a RegExp for concatenated expressions and for | (or) joined expressions.

---
+++ SablePP/SablePP.Generate/ConcatenatedRegularExpression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SablePP.Generate
+{
+    public class ConcatenatedRegularExpression : RegularExpression
+    {
+        private RegularExpression[] expressions;
+
+        public ConcatenatedRegularExpression(IEnumerable<RegularExpression> expressions)
+        {
+            if (expressions == null)
+                throw new ArgumentNullException("expressions");
+
+            this.expressions = expressions.ToArray();
+
+            if (this.expressions.Length == 0)
+                throw new ArgumentOutOfRangeException("A concatenated-regular-expression cannot consist of one regular expression.");
+        }
+
+        public RegularExpression[] Expressions
+        {
+            get { return expressions; }
+        }
+    }
+}
---
+++ SablePP/SablePP.Generate/OrRegularExpression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SablePP.Generate
+{
+    public class OrRegularExpression : RegularExpression
+    {
+        private RegularExpression[] expressions;
+
+        public OrRegularExpression(IEnumerable<RegularExpression> expressions)
+        {
+            if (expressions == null)
+                throw new ArgumentNullException("expressions");
+
+            this.expressions = expressions.ToArray();
+
+            if (this.expressions.Length == 0)
+                throw new ArgumentOutOfRangeException("An or-regular-expression cannot consist of one regular expression.");
+        }
+
+        public RegularExpression[] Expressions
+        {
+            get { return expressions; }
+        }
+    }
+}
