e0e826f9687d0682dd7ee9a918feaf890b5ecf98
deaddog <mikkelslsl@gmail.com>
2014-08-22 09:36:13 +0200

    Adds a Modifiers enum to the SablePP.Generate project.

---
+++ SablePP/SablePP.Generate/Modifiers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SablePP.Generate
+{
+    public enum Modifiers
+    {
+        Optional,
+        ZeroOrMany,
+        OneOrMany
+    }
+
+    public static class ModifiersExtension
+    {
+        public static char ToChar(this Modifiers modifier)
+        {
+            switch (modifier)
+            {
+                case Modifiers.Optional:
+                    return '?';
+                case Modifiers.ZeroOrMany:
+                    return '*';
+                case Modifiers.OneOrMany:
+                    return '+';
+
+                default:
+                    throw new ArgumentOutOfRangeException("modifier", "Unknown modifier.");
+            }
+        }
+    }
+}
