30aee5c43cad152e937808842aa5de9cf6bc732a
deaddog <mikkelslsl@gmail.com>
2014-08-22 09:39:07 +0200

    Implemens a RegExp for applying a modifier to a regular expression.

---
+++ SablePP/SablePP.Generate/ModifiedRegularExpression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SablePP.Generate
+{
+    class ModifiedRegularExpression : RegularExpression
+    {
+        private RegularExpression expression;
+        private Modifiers modifier;
+
+        public ModifiedRegularExpression(RegularExpression expression, Modifiers modifier)
+        {
+            if (expression == null)
+                throw new ArgumentNullException("expression");
+
+            if (modifier != Modifiers.Optional && modifier != Modifiers.OneOrMany && modifier != Modifiers.ZeroOrMany)
+                throw new ArgumentOutOfRangeException("modifier", "Unknown modifier.");
+
+            this.expression = expression;
+            this.modifier = modifier;
+        }
+
+        public RegularExpression Expression
+        {
+            get { return expression; }
+        }
+
+        public Modifiers Modifier
+        {
+            get { return modifier; }
+        }
+    }
+}
