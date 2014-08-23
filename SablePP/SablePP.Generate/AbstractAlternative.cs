ab60050d882bd2dcf51aba7a22311858bcc2159a
deaddog <mikkelslsl@gmail.com>
2014-08-23 12:09:21 +0200

    Implements the alternative and abstract alternative classes.

--- SablePP/SablePP.Generate/AbstractAlternative.cs
+++ SablePP/SablePP.Generate/AbstractAlternative.cs
@@ -1,12 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SablePP.Generate
 {
     public class AbstractAlternative
     {
+        private string name;
+        private Element[] elements;
+
+        public AbstractAlternative(string name, IEnumerable<Element> elements)
+        {
+            this.name = name;
+            this.elements = elements.ToArray();
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public Element[] Elements
+        {
+            get { return elements; }
+        }
+
+        public abstract class Element
+        {
+            private string name;
+            private Modifiers? modifier;
+
+            public Element(string name, Modifiers? modifier)
+            {
+                this.name = name;
+                this.modifier = modifier;
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public Modifiers? Modifier
+            {
+                get { return modifier; }
+            }
+        }
+
+        public class TokenElement : Element
+        {
+            private Token token;
+
+            public TokenElement(string name, Token token)
+                : base(name, null)
+            {
+                this.token = token;
+            }
+            public TokenElement(string name, Token token, Modifiers modifier)
+                : base(name, modifier)
+            {
+                this.token = token;
+            }
+
+            public Token Token
+            {
+                get { return token; }
+            }
+        }
+
+        public class ProductionElement : Element
+        {
+            private AbstractProduction production;
+
+            public ProductionElement(string name, AbstractProduction production)
+                : base(name, null)
+            {
+                this.production = production;
+            }
+            public ProductionElement(string name, AbstractProduction production, Modifiers modifier)
+                : base(name, modifier)
+            {
+                this.production = production;
+            }
+
+            public AbstractProduction Production
+            {
+                get { return production; }
+            }
+        }
     }
 }
--- SablePP/SablePP.Generate/Alternative.cs
+++ SablePP/SablePP.Generate/Alternative.cs
@@ -1,12 +1,85 @@
-﻿using System;
+﻿using SablePP.Generate.Translations;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SablePP.Generate
 {
     public class Alternative
     {
+        private Element[] elements;
+        private Translation translation;
+
+        public Alternative(IEnumerable<Element> elements, Translation translation)
+        {
+            this.elements = elements.ToArray();
+            this.translation = translation;
+        }
+
+        public Element[] Elements
+        {
+            get { return elements; }
+        }
+
+        public Translation Translation
+        {
+            get { return translation; }
+        }
+
+        public abstract class Element
+        {
+            private Modifiers? modifier;
+
+            public Element(Modifiers? modifier)
+            {
+                this.modifier = modifier;
+            }
+
+            public Modifiers? Modifier
+            {
+                get { return modifier; }
+            }
+        }
+
+        public class TokenElement : Element
+        {
+            private Token token;
+
+            public TokenElement(Token token)
+                : base(null)
+            {
+                this.token = token;
+            }
+            public TokenElement(Token token, Modifiers modifier)
+                : base(modifier)
+            {
+                this.token = token;
+            }
+
+            public Token Token
+            {
+                get { return token; }
+            }
+        }
+
+        public class ProductionElement : Element
+        {
+            private AbstractProduction production;
+
+            public ProductionElement(AbstractProduction production)
+                : base(null)
+            {
+                this.production = production;
+            }
+            public ProductionElement(AbstractProduction production, Modifiers modifier)
+                : base(modifier)
+            {
+                this.production = production;
+            }
+
+            public AbstractProduction Production
+            {
+                get { return production; }
+            }
+        }
     }
 }
