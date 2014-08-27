a5bad5aa45c937074c06aa0021952c99ce460433
deaddog <mikkelslsl@gmail.com>
2014-08-27 15:54:55 +0200

    Adds a class 'GrammarBuilder' to SablePP.Compiler that will generate a SablePP.Generate.Grammar structure.

---
+++ SablePP/SablePP.Compiler/GrammarBuilder.cs
@@ -0,0 +1,49 @@
+﻿using SablePP.Compiler.Nodes;
+using SablePP.Generate;
+using SablePP.Generate.RegularExpressions;
+using SablePP.Tools.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SablePP.Compiler
+{
+    public class GrammarBuilder
+    {
+        private Dictionary<PHelper, Helper> helpers;
+        private GrammarBuilder()
+        {
+            this.helpers = new Dictionary<PHelper, Helper>();
+        }
+
+        public static Grammar BuildSableCCGrammar(Start<PGrammar> grammar)
+        {
+            GrammarBuilder builder = new GrammarBuilder();
+            return builder.Visit(grammar.Root as AGrammar);
+        }
+
+        public Grammar Visit(AGrammar node)
+        {
+            var package = node.PackageName;
+            var helpers = Visit(node.Helpers).ToArray();
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Helper> Visit(PHelpers node)
+        {
+            return from h in node.Helpers select Visit(h);
+        }
+
+        public Helper Visit(PHelper node)
+        {
+            var helper = new Helper(Visit(node.Regex));
+            helpers.Add(node, helper);
+            return helper;
+        }
+
+        public RegExp Visit(PRegex node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
