e371c57aa3ca47618e874ddc93c25d9674b9becf
deaddog <mikkelslsl@gmail.com>
2014-08-27 16:36:05 +0200

    Creates a list to which data can be added, but not removed.

---
+++ SablePP/SablePP.Generate/AddOnlyList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Generate
+{
+    public class AddOnlyList<T>
+    {
+        private List<T> innerList;
+
+        public AddOnlyList()
+        {
+            this.innerList = new List<T>();
+        }
+
+        public AddOnlyList(IEnumerable<T> items)
+        {
+            this.innerList = new List<T>(items);
+        }
+
+        public int Count
+        {
+            get { return innerList.Count; }
+        }
+
+        public T this[int index]
+        {
+            get { return innerList[index]; }
+        }
+
+        public void Add(T item)
+        {
+            innerList.Add(item);
+        }
+        public void AddRange(IEnumerable<T> items)
+        {
+            innerList.AddRange(items);
+        }
+        public void Insert(int index, T item)
+        {
+            innerList.Insert(index, item);
+        }
+    }
+}
