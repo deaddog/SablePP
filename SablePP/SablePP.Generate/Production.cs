de67bc6a69ea1567a7128065095629d40457fb80
deaddog <mikkelslsl@gmail.com>
2014-08-28 09:23:28 +0200

    A production translation in SablePP.Generate can be a translation to a token.

--- SablePP/SablePP.Generate/Production.cs
+++ SablePP/SablePP.Generate/Production.cs
@@ -38,18 +38,46 @@ namespace SablePP.Generate
         public class ProductionTranslation
         {
             private AbstractProduction production;
+            private Token token;
             private Modifiers? modifier;
 
+            public ProductionTranslation(Token token, Modifiers? modifier)
+            {
+                if (token == null)
+                    throw new ArgumentNullException("token");
+
+                this.production = null;
+                this.token = token;
+                this.modifier = modifier;
+            }
             public ProductionTranslation(AbstractProduction production, Modifiers? modifier)
             {
+                if (production == null)
+                    throw new ArgumentNullException("production");
+
                 this.production = production;
+                this.token = null;
                 this.modifier = modifier;
+            }
+
+            public bool HasProduction
+            {
+                get { return production != null; }
+            }
+            public bool HasToken
+            {
+                get { return token != null; }
             }
 
             public AbstractProduction Production
             {
                 get { return production; }
             }
+            public Token Token
+            {
+                get { return token; }
+            }
+
             public Modifiers? Modifier
             {
                 get { return modifier; }
