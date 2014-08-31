866c03011bf6cb21ee7a4d528401dc1621b4365b
deaddog <mikkelslsl@gmail.com>
2014-08-31 21:46:04 +0200

    Adds a PAlternative.Target to the PTranslation.

--- SablePP/SablePP.Compiler/Nodes/PTranslation.cs
+++ SablePP/SablePP.Compiler/Nodes/PTranslation.cs
@@ -1,7 +1,22 @@
-﻿namespace SablePP.Compiler.Nodes
+﻿using System;
+
+namespace SablePP.Compiler.Nodes
 {
     public partial class PTranslation
     {
+        private PAlternative.Target target;
+        public PAlternative.Target Target
+        {
+            get { return target; }
+            set
+            {
+                if (target != null)
+                    throw new InvalidOperationException("Cannot set translation target twice.");
+
+                target = value;
+            }
+        }
+
         public abstract string ClassName
         {
             get;
