98bfa457f284844b60480b0acb4b545d86e4445a
deaddog <mikkelslsl@gmail.com>
2014-08-31 16:32:44 +0200

    PAlternative uses a Target declaration similar to PProduction.

--- SablePP/SablePP.Compiler/Nodes/PAlternative.cs
+++ SablePP/SablePP.Compiler/Nodes/PAlternative.cs
@@ -30,15 +30,66 @@ namespace SablePP.Compiler.Nodes
                 return null;
         }
 
-        private AAlternative astTarget;
-        public AAlternative AstTarget
+        private Target astTarget;
+        public Target AstTarget
         {
             get { return astTarget; }
             set
             {
                 if (astTarget != null)
-                    throw new InvalidOperationException("Cannot set target alternative twice.");
+                    throw new InvalidOperationException("Cannot set production target twice.");
+
                 astTarget = value;
+            }
+        }
+
+        public class Target
+        {
+            private PAlternative alternative;
+            private PToken token;
+
+            private Modifiers modifier;
+
+            public Target(PAlternative alternative, Modifiers modifier)
+            {
+                if (alternative == null)
+                    throw new ArgumentNullException("alternative");
+
+                this.alternative = alternative;
+                this.token = null;
+                this.modifier = modifier;
+            }
+            public Target(PToken token, Modifiers modifier)
+            {
+                if (token == null)
+                    throw new ArgumentNullException("token");
+
+                this.alternative = null;
+                this.token = token;
+                this.modifier = modifier;
+            }
+
+            public bool IsAlternative
+            {
+                get { return alternative != null; }
+            }
+            public bool IsToken
+            {
+                get { return token != null; }
+            }
+
+            public PAlternative Alternative
+            {
+                get { return alternative; }
+            }
+            public PToken Token
+            {
+                get { return token; }
+            }
+
+            public Modifiers Modifier
+            {
+                get { return modifier; }
             }
         }
     }
