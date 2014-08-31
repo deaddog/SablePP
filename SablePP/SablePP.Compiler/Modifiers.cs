8444da4e709e4bb83b55c6ed92a0ed94e22d3a24
deaddog <mikkelslsl@gmail.com>
2014-08-31 16:27:51 +0200

    Implements an extension method on the Modifiers enum that gets a Modifiers value from a PModifier node.

--- SablePP/SablePP.Compiler/Modifiers.cs
+++ SablePP/SablePP.Compiler/Modifiers.cs
@@ -1,4 +1,7 @@
-﻿namespace SablePP.Compiler
+﻿using SablePP.Compiler.Nodes;
+using System;
+
+namespace SablePP.Compiler
 {
     public enum Modifiers
     {
@@ -6,5 +9,22 @@
         Optional,
         OneOrMany,
         ZeroOrMany
+    }
+
+    public static class ModifiersExtension
+    {
+        public static Modifiers GetModifier(this PModifier node)
+        {
+            if (node == null)
+                return Modifiers.Single;
+            if (node is AQuestionModifier)
+                return Modifiers.Optional;
+            if (node is APlusModifier)
+                return Modifiers.OneOrMany;
+            if (node is AStarModifier)
+                return Modifiers.ZeroOrMany;
+
+            throw new ArgumentException("Unable to determine modifier for " + node);
+        }
     }
 }
