edab0b39f6f2aa1f7e18d4d007ca19158d0bef7e
deaddog <mikkelslsl@gmail.com>
2014-09-08 19:20:30 +0200

    Creates the SablePP.Generate.SableCC.Builder class stub, that will generate a grammar for SableCC to process.

---
+++ SablePP/SablePP.Generate/SableCC/Builder.cs
@@ -0,0 +1,30 @@
+﻿using SablePP.Tools.Generate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Generate.SableCC
+{
+    internal class Builder
+    {
+        private PatchElement root;
+
+        private Builder()
+        {
+            this.root = new PatchElement();
+        }
+
+        public static void BuildToFile(Grammar grammar, string filepath)
+        {
+            Builder builder = new Builder();
+            builder.Start(grammar);
+            builder.root.ToFile(filepath, Encoding.Unicode, "  ");
+        }
+
+        private void Start(Grammar grammar)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
