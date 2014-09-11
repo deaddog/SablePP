c5a40f9a5adb5b9b1b27831b2897ce8a188c71b3
deaddog <mikkelslsl@gmail.com>
2014-09-11 12:21:04 +0200

    Adds temporary path setup to the SablePP.Generate.SableCC.Compiler class (from the PathInformation class).

--- SablePP/SablePP.Generate/SableCC/Compiler.cs
+++ SablePP/SablePP.Generate/SableCC/Compiler.cs
@@ -1,9 +1,90 @@
 ﻿using Microsoft.Win32;
+using System;
+using System.IO;
 
 namespace SablePP.Generate.SableCC
 {
     internal static class Compiler
     {
+        #region TempPath
+
+        private static string _executing_ = null;
+        private static string _temporary_ = null;
+
+        private static object pathLock = new object();
+
+        static Compiler()
+        {
+            // Sets executing directory
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            Uri uri = new Uri(assembly.CodeBase);
+            _executing_ = Path.GetDirectoryName(uri.LocalPath);
+
+            // Sets temporary directory
+            string tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName().Replace(".", ""));
+
+            if(!Directory.Exists(tempDir))
+                Directory.CreateDirectory(tempDir);
+
+            _temporary_ = tempDir;
+        }
+
+        public static string ExecutingDirectory
+        {
+            get { return _executing_; }
+        }
+
+        public static void CleanTemporaryFiles()
+        {
+            lock (pathLock)
+                if (_temporary_ == null)
+                    return;
+                else
+                {
+                    if (Directory.Exists(_temporary_))
+                        Directory.Delete(_temporary_, true);
+
+                    _temporary_ = null;
+                }
+        }
+
+        public static string TemporaryDirectory
+        {
+            get
+            {
+                if (_temporary_ == null)
+                    throw new InvalidOperationException("TemporaryDirectory cannot be access after CleanTemporaryFiles has been called.");
+
+                return _temporary_;
+            }
+        }
+        public static string SableOutputDirectory
+        {
+            get
+            {
+                string sableDir = Path.Combine(TemporaryDirectory, "sable");
+
+                DirectoryInfo dir = new DirectoryInfo(sableDir);
+                if (!dir.Exists)
+                    dir.Create();
+
+                return sableDir;
+            }
+        }
+
+        public static string TemporaryGrammarPath
+        {
+            get { return TemporaryDirectory + "\\grammar.sablecc"; }
+        }
+        public static string TemporarySableGrammarPath
+        {
+            get { return SableOutputDirectory + "\\grammar.sablecc"; }
+        }
+
+        #endregion
+
+        #region Java
+
         private static bool _javaLoaded_ = false;
         private static void loadJavaInformation()
         {
@@ -69,5 +150,7 @@ namespace SablePP.Generate.SableCC
                 return _javaFound_;
             }
         }
+
+        #endregion
     }
 }
