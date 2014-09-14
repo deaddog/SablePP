3082f514b653a4ffcc513b250294c84142fbd10a
deaddog <mikkelslsl@gmail.com>
2014-09-14 15:15:24 +0200

    Adds methods and fields in CompilationResult for getting and setting lexer/parser tables. This is all defined as 'internal' in SablePP.Generate.

--- SablePP/SablePP.Generate/CompilationResult.cs
+++ SablePP/SablePP.Generate/CompilationResult.cs
@@ -1,8 +1,15 @@
-﻿namespace SablePP.Generate
+﻿using SablePP.Tools.Generate;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SablePP.Generate
 {
     public class CompilationResult
     {
         private CompilationError error;
+
+        private PatchElement lexerGoto, lexerAccept, parserAction, parserGoto, parserMessage, parserError;
 
         internal CompilationResult()
             : this(null)
@@ -16,6 +23,77 @@
         public CompilationError Error
         {
             get { return error; }
+        }
+
+        internal void SetLexerTables(string lexerCode)
+        {
+            lexerGoto = getTable(lexerCode, @"int\[\]\[\]\[\]\[\] gotoTable[^{]+(?<table>{[^;]*);");
+            lexerAccept = getTable(lexerCode, @"int\[\]\[\] accept[^{]+(?<table>{[^;]*);");
+        }
+        internal void SetParserTables(string parserCode)
+        {
+            parserAction = getTable(parserCode, @"int\[\]\[\]\[\] actionTable[^{]+(?<table>{[^;]*);");
+            parserGoto = getTable(parserCode, @"int\[\]\[\]\[\] gotoTable[^{]+(?<table>{[^;]*);");
+            parserMessage = getTable(parserCode, "String\\[\\] errorMessages[^{]*(?<table>([^\"}]*\"[^\"]*\")+[^}]*\\});");
+            parserError = getTable(parserCode, @"int\[\] errors[^{]+(?<table>{[^;]*);");
+        }
+        private static PatchElement getTable(string code, string regex)
+        {
+            PatchElement element = new PatchElement();
+
+            code = Regex.Match(code, regex).Groups["table"].Value;
+
+            string[] strings = getNonEmpty(code.Split(new char[] { '\r', '\n' })).ToArray();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].StartsWith("}"))
+                    element.DecreaseIndentation();
+
+                element.Emit(strings[i]);
+                if (i < strings.Length - 1)
+                    element.EmitNewLine();
+
+                if (strings[i].EndsWith("{"))
+                    element.IncreaseIndentation();
+            }
+
+            return element;
+        }
+        private static IEnumerable<string> getNonEmpty(IEnumerable<string> collection)
+        {
+            foreach (var s in collection)
+            {
+                var t = s.Trim();
+                if (t.Length > 0)
+                    yield return t;
+            }
+        }
+
+        internal PatchElement LexerGotoTable
+        {
+            get { return lexerGoto; }
+        }
+        internal PatchElement LexerAcceptTable
+        {
+            get { return lexerAccept; }
+        }
+
+        internal PatchElement ParserActionTable
+        {
+            get { return parserAction; }
+        }
+        internal PatchElement ParserGotoTable
+        {
+            get { return parserGoto; }
+        }
+        internal PatchElement ParserErrorMessagesTable
+        {
+            get { return parserMessage; }
+        }
+        internal PatchElement ParserErrorTable
+        {
+            get { return parserError; }
         }
     }
 }
