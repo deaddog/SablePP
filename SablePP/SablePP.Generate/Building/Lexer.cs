575286081228fd639f5aba22920b76f1e1d8699c
deaddog <mikkelslsl@gmail.com>
2014-09-19 07:19:40 +0200

    Adds a class stub that handles the Lexer generation.

---
+++ SablePP/SablePP.Generate/Building/Lexer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Generate.Building
+{
+    public class Lexer
+    {
+        private Grammar grammar;
+        private CompilationResult tables;
+
+        public Lexer(Grammar grammar, CompilationResult tables)
+        {
+            this.grammar = grammar;
+            this.tables = tables;
+        }
+    }
+}
