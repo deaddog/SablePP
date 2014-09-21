fec66690a29e670ca65a39f0f6cea9c30840c25c
deaddog <mikkelslsl@gmail.com>
2014-09-21 20:37:33 +0200

    Creates a 'NameTable' type for the SablePP builder classes.

---
+++ SablePP/SablePP.Generate/Building/NameTable.cs
@@ -0,0 +1,31 @@
+﻿using SablePP.Tools;
+using System.Collections.Generic;
+
+namespace SablePP.Generate.Building
+{
+    internal class NameTable<T>
+    {
+        private Dictionary<string, T> objects;
+        private Dictionary<T, string> names;
+        private SafeNameDictionary<T> safe;
+
+        public NameTable()
+        {
+            this.objects = new Dictionary<string,T>();
+            this.names = new Dictionary<T, string>();
+            this.safe = new SafeNameDictionary<T>(this.objects);
+        }
+
+        public string Add(string name, T obj)
+        {
+            name = safe.Add(name, obj);
+            names.Add(obj, name);
+            return name;
+        }
+
+        public string this[T obj]
+        {
+            get { return names[obj]; }
+        }
+    }
+}
