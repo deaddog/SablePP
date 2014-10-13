e3bbb36c7db1cfb61119e9511cc1552f3f1af0aa
deaddog <mikkelslsl@gmail.com>
2014-10-13 22:37:59 +0200

    Added a class for generating token nodes.

--- SablePP/SablePP.Generate/Building/Lexer.cs
+++ SablePP/SablePP.Generate/Building/Lexer.cs
@@ -1,12 +1,7 @@
-﻿using SablePP.Tools;
-using SablePP.Tools.Generate;
+﻿using SablePP.Tools.Generate;
 using SablePP.Tools.Generate.CSharp;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace SablePP.Generate.Building
 {
--- SablePP/SablePP.Generate/Building/Parser.cs
+++ SablePP/SablePP.Generate/Building/Parser.cs
@@ -3,7 +3,6 @@ using SablePP.Tools.Generate.CSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace SablePP.Generate.Building
 {
