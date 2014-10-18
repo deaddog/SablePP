6b2a2c2f0c359aa9f9a32b90015238fd8ad42aff
deaddog <mikkelslsl@gmail.com>
2014-10-19 01:06:09 +0200

    Copied the code from the original ProductionNodes builder into the new one.

--- SablePP/SablePP.Generate/Building/BProduction.cs
+++ SablePP/SablePP.Generate/Building/BProduction.cs
@@ -8,5 +8,71 @@ namespace SablePP.Generate.Building
 {
     internal class BProduction
     {
+        private FileElement fileElement;
+        private NameSpaceElement nameElement;
+        private ClassElement classElement;
+        private string productionName;
+
+        private ProductionNodes()
+        {
+            fileElement = new FileElement();
+            fileElement.Using.Add("System");
+            fileElement.Using.Add("System.Collections.Generic");
+            fileElement.Using.Add(SablePP.Generate.Namespaces.Nodes);
+        }
+
+        public static FileElement BuildCode(Start<PGrammar> astRoot)
+        {
+            ProductionNodes n = new ProductionNodes();
+            n.Visit(astRoot);
+            return n.fileElement;
+        }
+
+        public override void CaseAGrammar(AGrammar node)
+        {
+            if (node.HasPackage)
+                Visit(node.Package);
+
+            string packageName = node.PackageName;
+
+            fileElement.Add(nameElement = new NameSpaceElement(packageName + ".Nodes"));
+            fileElement.Using.Add(packageName + ".Analysis");
+
+            if (node.HasAstproductions)
+                Visit(node.Astproductions);
+            else if (node.HasProductions)
+                Visit(node.Productions);
+        }
+
+        public override void CaseAProduction(AProduction node)
+        {
+            this.productionName = ToCamelCase(node.Identifier.Text);
+            string name = "P" + productionName;
+
+            nameElement.Add(classElement = new ClassElement("public abstract partial class {0} : Production<{0}>", name));
+
+            FieldBuilder.Emit(classElement, node);
+            ConstructorBuilder.Emit(classElement, node);
+            PropertiesBuilder.Emit(classElement, node);
+
+            base.CaseAProduction(node);
+        }
+
+        public override void CaseAAlternative(AAlternative node)
+        {
+            string name = "A" + productionName;
+            if (node.HasAlternativename)
+                name = "A" + ToCamelCase((node.Alternativename as AAlternativename).Name.Text) + productionName;
+
+            nameElement.Add(classElement = new ClassElement("public partial class {0} : P{1}", name, productionName));
+
+            FieldBuilder.Emit(classElement, node);
+            ConstructorBuilder.Emit(classElement, node);
+            PropertiesBuilder.Emit(classElement, node);
+            ReplaceMethodBuilder.Emit(classElement, node);
+            GetChildrenMethodBuilder.Emit(classElement, node);
+            CloneMethodBuilder.Emit(classElement, node);
+            ToStringMethodBuilder.Emit(classElement, node);
+        }
     }
 }
