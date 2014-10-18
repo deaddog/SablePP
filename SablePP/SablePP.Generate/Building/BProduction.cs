0fbc2589b3b302cb11d2e16cd1e5499288ce520b
deaddog <mikkelslsl@gmail.com>
2014-10-19 00:54:30 +0200

    Created a class stub for building Production nodes.

---
+++ SablePP/SablePP.Generate/Building/BProduction.cs
@@ -0,0 +1,12 @@
+﻿using SablePP.Tools.Generate.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Generate.Building
+{
+    internal class BProduction
+    {
+    }
+}
