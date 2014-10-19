5adee120d7002e4967370a7c26029c8211839c1f
deaddog <mikkelslsl@gmail.com>
2014-10-19 02:48:26 +0200

    A reference to the stack top is maintained to ease access.

--- SablePP/SablePP.Tools/SafeNameDictionary.cs
+++ SablePP/SablePP.Tools/SafeNameDictionary.cs
@@ -6,6 +6,7 @@ namespace SablePP.Tools
     public class SafeNameDictionary
     {
         private Stack<DictionarySet> stack;
+        private DictionarySet peek;
         private SafeName safename;
 
         public SafeNameDictionary()
@@ -15,9 +16,14 @@ namespace SablePP.Tools
         public SafeNameDictionary(Func<string, IEnumerable<string>> getTestNames)
         {
             this.stack = new Stack<DictionarySet>();
-            this.safename = new SafeName(x => stack.Peek().NameToItem.ContainsKey(x), getTestNames ?? SafeName.GetNumberedNames);
+            this.safename = new SafeName(containsName, getTestNames ?? SafeName.GetNumberedNames);
 
-            this.stack.Push(new DictionarySet());
+            this.stack.Push(peek = new DictionarySet());
+        }
+
+        private bool containsName(string name)
+        {
+            return peek.NameToItem.ContainsKey(name);
         }
 
         public int ScopeLevel
@@ -27,19 +33,23 @@ namespace SablePP.Tools
 
         public void OpenScope()
         {
-            stack.Push(new DictionarySet());
+            stack.Push(peek = new DictionarySet());
         }
         public void CloseScope()
         {
             if (stack.Count == 1)
                 throw new InvalidOperationException("Cannot close the outermost scope in a SafeNameDictionary.");
             else
+            {
                 stack.Pop();
+                peek = stack.Peek();
+            }
         }
         public void CloseAllScopes()
         {
             while (stack.Count > 1)
                 stack.Pop();
+            peek = stack.Peek();
         }
 
         private class DictionarySet
