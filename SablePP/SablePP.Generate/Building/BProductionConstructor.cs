2543f1161a8d45e18c801286d16f655547666fdd
deaddog <mikkelslsl@gmail.com>
2014-10-19 14:46:35 +0200

    Copied the contents of the old constructor builder.

--- SablePP/SablePP.Generate/Building/BProductionConstructor.cs
+++ SablePP/SablePP.Generate/Building/BProductionConstructor.cs
@@ -7,5 +7,77 @@ namespace SablePP.Generate.Building
 {
     internal partial class BProduction
     {
+        private MethodElement constructor;
+
+        public static void Emit(ClassElement classElement, AProduction node)
+        {
+            var fields = ProductionElement.GetSharedElements(node);
+
+            emit(classElement, fields, null, fields);
+            classElement.EmitNewline();
+        }
+        public static void Emit(ClassElement classElement, AAlternative node)
+        {
+            emit(classElement, ProductionElement.GetAllElements(node), ProductionElement.GetSharedElements(node), ProductionElement.GetUniqueElements(node));
+            classElement.EmitNewline();
+        }
+
+        private static void emit(ClassElement classElement, ProductionElement[] parameters, ProductionElement[] chain, ProductionElement[] fields)
+        {
+            string chainCall = null;
+            if (chain != null)
+            {
+                chainCall = "base(";
+                for (int i = 0; i < chain.Length; i++)
+                {
+                    if (i > 0)
+                        chainCall += ", ";
+                    chainCall += chain[i].FieldName;
+                }
+                chainCall += ")";
+            }
+
+            var constructor = new MethodElement("public {0}()", chainCall, true, classElement.Name);
+            ConstructorBuilder builder = new ConstructorBuilder() { constructor = constructor };
+
+            classElement.Add(constructor);
+
+            foreach (var e in fields)
+                builder.emitAssignment(e);
+            foreach (var e in parameters)
+                builder.emitParameter(e);
+        }
+
+        private void emitAssignment(ProductionElement node)
+        {
+            switch (node.ElementType)
+            {
+                case ElementTypes.Simple:
+                case ElementTypes.Question:
+                    constructor.Body.EmitLine("this.{0} = {1};", node.PropertyName, node.FieldName);
+                    break;
+                case ElementTypes.Plus:
+                case ElementTypes.Star:
+                    constructor.Body.EmitLine("this.{0} = new NodeList<{1}>(this, {0}, {2});",
+                        node.FieldName,
+                        node.ProductionOrTokenClass,
+                        node.ElementType == ElementTypes.Star ? "true" : "false");
+                    break;
+            }
+        }
+        private void emitParameter(ProductionElement node)
+        {
+            switch (node.ElementType)
+            {
+                case ElementTypes.Simple:
+                case ElementTypes.Question:
+                    constructor.Parameters.Add("{0} {1}", node.ProductionOrTokenClass, node.FieldName);
+                    break;
+                case ElementTypes.Plus:
+                case ElementTypes.Star:
+                    constructor.Parameters.Add("IEnumerable<{0}> {1}", node.ProductionOrTokenClass, node.FieldName);
+                    break;
+            }
+        }
     }
 }
