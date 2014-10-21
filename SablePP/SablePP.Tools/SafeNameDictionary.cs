973b6200876c86ce45d183d5acaef8ec46b71893
deaddog <mikkelslsl@gmail.com>
2014-10-21 07:12:32 +0200

    Created constructors for the non-generic SafeNameDictionary.

--- SablePP/SablePP.Tools/SafeNameDictionary.cs
+++ SablePP/SablePP.Tools/SafeNameDictionary.cs
@@ -14,7 +14,7 @@ namespace SablePP.Tools
         private SafeName safename, allscopesSafeName;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SafeNameDictionary"/> class that is empty,
+        /// Initializes a new instance of the <see cref="SafeNameDictionary{T}"/> class that is empty,
         /// has the default initial capacity,
         /// and uses <see cref="SafeName.GetNumberedNames"/> to generate variable names.
         /// </summary>
@@ -23,11 +23,11 @@ namespace SablePP.Tools
         {
         }
         /// <summary>
-        /// Initializes a new instance of the <see cref="SafeNameDictionary"/> class that is empty,
+        /// Initializes a new instance of the <see cref="SafeNameDictionary{T}"/> class that is empty,
         /// has the default initial capacity,
         /// and uses a function to generate variable names.
         /// </summary>
-        /// <param name="getTestNames">A <see cref="NameGenerator"/> that generates names for the <see cref="SafeNameDictionary"/>.</param>
+        /// <param name="getTestNames">A <see cref="NameGenerator"/> that generates names for the <see cref="SafeNameDictionary{T}"/>.</param>
         public SafeNameDictionary(NameGenerator getTestNames)
         {
             this.stack = new Stack<DictionarySet>();
@@ -38,11 +38,11 @@ namespace SablePP.Tools
         }
 
         /// <summary>
-        /// Adds the first unused name, matching a seed name, to this <see cref="SafeNameDictionary"/>.
+        /// Adds the first unused name, matching a seed name, to this <see cref="SafeNameDictionary{T}"/>.
         /// </summary>
         /// <param name="name">The name seed from which names are generated.</param>
         /// <param name="allscopes">if set to <c>true</c> the generated name is unused in all scopes; otherwise, it is only unsued in the topmost scope.</param>
-        /// <returns>The name that was inserted in the <see cref="SafeNameDictionary"/></returns>
+        /// <returns>The name that was inserted in the <see cref="SafeNameDictionary{T}"/></returns>
         public string Add(string name, bool allscopes = true)
         {
             name = (allscopes ? allscopesSafeName : safename).GetName(name);
@@ -50,13 +50,13 @@ namespace SablePP.Tools
             return name;
         }
         /// <summary>
-        /// Adds the first unused name, matching a seed name, to this <see cref="SafeNameDictionary"/>.
+        /// Adds the first unused name, matching a seed name, to this <see cref="SafeNameDictionary{T}"/>.
         /// Stores a two-way link between that name and <paramref name="item"/> such that one can be retrieved from the other.
         /// </summary>
         /// <param name="name">The name seed from which names are generated.</param>
         /// <param name="item">The item that should be associated with the generated name.</param>
         /// <param name="allscopes">if set to <c>true</c> the generated name is unused in all scopes; otherwise, it is only unsued in the topmost scope.</param>
-        /// <returns>The name that was inserted in the <see cref="SafeNameDictionary"/>
+        /// <returns>The name that was inserted in the <see cref="SafeNameDictionary{T}"/>
         /// or the existing name stored for <paramref name="item"/> (if it exists).
         /// The latter disregards the <paramref name="name"/> parameter.</returns>
         public string Add(string name, T item, bool allscopes = true)
@@ -89,7 +89,7 @@ namespace SablePP.Tools
         /// </summary>
         /// <param name="name">The name for which an item should be extracted.</param>
         /// <returns>The item associated with <paramref name="name"/>
-        /// or <c>null</c> if <paramref name="name"/> was known by the <see cref="SafeNameDictionary"/> but not associated with an item.
+        /// or <c>null</c> if <paramref name="name"/> was known by the <see cref="SafeNameDictionary{T}"/> but not associated with an item.
         /// The item returned will be from the topmost scope containing <paramref name="name"/>.</returns>
         public T this[string name]
         {
@@ -119,11 +119,11 @@ namespace SablePP.Tools
         }
 
         /// <summary>
-        /// Determines whether the <see cref="SafeNameDictionary"/> contains the specified name.
+        /// Determines whether the <see cref="SafeNameDictionary{T}"/> contains the specified name.
         /// </summary>
         /// <param name="name">The name to find an item for.</param>
         /// <param name="allscopes">if set to <c>true</c> all scopes are checked for <paramref name="name"/>; otherwise only the topmost scope is checked.</param>
-        /// <returns><c>true</c> if the specified name was found in the <see cref="SafeNameDictionary"/>; otherwise <c>false</c>.</returns>
+        /// <returns><c>true</c> if the specified name was found in the <see cref="SafeNameDictionary{T}"/>; otherwise <c>false</c>.</returns>
         public bool ContainsName(string name, bool allscopes)
         {
             if (allscopes)
@@ -137,11 +137,11 @@ namespace SablePP.Tools
                 return peek.NameToItem.ContainsKey(name);
         }
         /// <summary>
-        /// Determines whether the <see cref="SafeNameDictionary"/> contains a name for the specified item.
+        /// Determines whether the <see cref="SafeNameDictionary{T}"/> contains a name for the specified item.
         /// </summary>
         /// <param name="item">The item to find a name for.</param>
         /// <param name="allscopes">if set to <c>true</c> all scopes are checked for <paramref name="item"/>; otherwise only the topmost scope is checked.</param>
-        /// <returns><c>true</c> if the specified item was found in the <see cref="SafeNameDictionary"/>; otherwise <c>false</c>.</returns>
+        /// <returns><c>true</c> if the specified item was found in the <see cref="SafeNameDictionary{T}"/>; otherwise <c>false</c>.</returns>
         public bool ContainsItem(T item, bool allscopes)
         {
             if (allscopes)
@@ -161,7 +161,7 @@ namespace SablePP.Tools
         /// <param name="name">The name for which an item should be extracted.</param>
         /// <param name="allscopes">if set to <c>true</c> all scopes are checked for <paramref name="name"/>; otherwise only the topmost scope is checked.</param>
         /// <returns>The <typeparamref name="T"/> item associated with <paramref name="name"/>
-        /// or <c>null</c> if <paramref name="name"/> was known by the <see cref="SafeNameDictionary"/> but not associated with an item.
+        /// or <c>null</c> if <paramref name="name"/> was known by the <see cref="SafeNameDictionary{T}"/> but not associated with an item.
         /// The item returned will be from the topmost scope containing <paramref name="name"/>.</returns>
         public T GetItem(string name, bool allscopes)
         {
@@ -270,6 +270,26 @@ namespace SablePP.Tools
     /// </summary>
     public class SafeNameDictionary : SafeNameDictionary<object>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SafeNameDictionary"/> class that is empty,
+        /// has the default initial capacity,
+        /// and uses <see cref="SafeName.GetNumberedNames"/> to generate variable names.
+        /// </summary>
+        public SafeNameDictionary()
+            : this(null)
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SafeNameDictionary"/> class that is empty,
+        /// has the default initial capacity,
+        /// and uses a function to generate variable names.
+        /// </summary>
+        /// <param name="getTestNames">A <see cref="NameGenerator"/> that generates names for the <see cref="SafeNameDictionary"/>.</param>
+        public SafeNameDictionary(NameGenerator getTestNames)
+            : base(getTestNames)
+        {
+        }
+
         /// <summary>
         /// Gets the item associated with the specified name.
         /// </summary>
