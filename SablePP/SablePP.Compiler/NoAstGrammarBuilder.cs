9ca6cce73e2814c58a1ce4ed870376607f96ebdf
deaddog <mikkelslsl@gmail.com>
2014-11-01 07:31:56 +0100

    Created two GrammarBuilders, that will specialize the current one depending on weather there is an AST section or not.

---
+++ SablePP/SablePP.Compiler/NoAstGrammarBuilder.cs
@@ -0,0 +1,15 @@
+﻿using SablePP.Compiler.Nodes;
+using SablePP.Generate;
+using SablePP.Generate.RegularExpressions;
+using SablePP.Generate.Translations;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace SablePP.Compiler
+{
+    public class NoAstGrammarBuilder : GrammarBuilder
+    {
+    }
+}
