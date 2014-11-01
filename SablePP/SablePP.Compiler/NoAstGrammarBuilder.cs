c9588c7de990a4cd46dcfe7cb0c2e7a9004413ca
deaddog <mikkelslsl@gmail.com>
2014-11-01 07:53:20 +0100

    Implemented the Visit(AGrammar) method in NoAstGrammarBuilder and added stubs for the required Visit methods.

--- SablePP/SablePP.Compiler/NoAstGrammarBuilder.cs
+++ SablePP/SablePP.Compiler/NoAstGrammarBuilder.cs
@@ -11,7 +11,60 @@ namespace SablePP.Compiler
 {
     public class NoAstGrammarBuilder : GrammarBuilder
     {
+        private Dictionary<PProduction, Production> productions;
+
+        private Dictionary<Production, AbstractProduction> abstractProductions;
+        private Dictionary<Alternative, AbstractAlternative> abstractAlternatives;
+
+        internal NoAstGrammarBuilder()
+        {
+            this.productions = new Dictionary<PProduction, Production>();
+
+            this.abstractProductions = new Dictionary<Production, AbstractProduction>();
+            this.abstractAlternatives = new Dictionary<Alternative, AbstractAlternative>();
+        }
+
         public override Grammar Visit(AGrammar node)
+        {
+            var _package = node.PackageName;
+            var _helpers = node.HasHelpers ? Visit(node.Helpers).ToArray() : new Helper[0];
+            var _states = node.HasStates ? Visit(node.States).ToArray() : new State[0];
+            var _tokens = node.HasTokens ? Visit(node.Tokens).ToArray() : new Token[0];
+
+            foreach (var p in node.Productions.Productions)
+            {
+                string name = "P" + p.Identifier.Text.ToCamelCase();
+                AbstractProduction abs = new AbstractProduction(name);
+
+                var prod = new Production(new Production.ProductionTranslation(abs, Modifiers.Single));
+                productions.Add(p, prod);
+                abstractProductions.Add(prod, abs);
+            }
+
+            var _productions = node.HasProductions ? Visit(node.Productions).ToArray() : new Production[0];
+            var _styles = node.HasHighlightrules ? Visit(node.Highlightrules).ToArray() : new Highlighting[0];
+
+            return new Grammar(_package, _helpers, _states, _tokens, _productions, abstractProductions.Values, _styles);
+        }
+
+        public IEnumerable<Production> Visit(PProductions node)
+        {
+            return from p in node.Productions select Visit(p);
+        }
+        public Production Visit(PProduction node)
+        {
+            var prod = productions[node];
+
+            prod.Alternatives.AddRange(from a in node.Alternatives select Visit(a));
+
+            return prod;
+        }
+
+        public Alternative Visit(PAlternative node)
+        {
+            throw new NotImplementedException();
+        }
+        public Alternative.Element Visit(PElement node)
         {
             throw new NotImplementedException();
         }
