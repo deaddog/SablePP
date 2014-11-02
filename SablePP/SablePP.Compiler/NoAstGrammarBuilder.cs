5ec465947d2d2203f28dbbe21ee272659a25509f
deaddog <mikkelslsl@gmail.com>
2014-11-02 08:29:22 +0100

    NoAstGrammarBuilder generates AST productions and alternatives, allowing for grammars without an AST section.

--- SablePP/SablePP.Compiler/NoAstGrammarBuilder.cs
+++ SablePP/SablePP.Compiler/NoAstGrammarBuilder.cs
@@ -41,32 +41,83 @@ namespace SablePP.Compiler
                 abstractProductions.Add(prod, abs);
             }
 
-            var _productions = node.HasProductions ? Visit(node.Productions).ToArray() : new Production[0];
+            foreach (var p in node.Productions.Productions)
+                Visit(p);
+
             var _styles = node.HasHighlightrules ? Visit(node.Highlightrules).ToArray() : new Highlighting[0];
 
-            return new Grammar(_package, _helpers, _states, _tokens, _productions, abstractProductions.Values, _styles);
+            return new Grammar(_package, _helpers, _states, _tokens, productions.Values, abstractProductions.Values, _styles);
         }
 
-        public IEnumerable<Production> Visit(PProductions node)
-        {
-            return from p in node.Productions select Visit(p);
-        }
-        public Production Visit(PProduction node)
+        public void Visit(PProduction node)
         {
             var prod = productions[node];
+            var abs = abstractProductions[prod];
 
-            prod.Alternatives.AddRange(from a in node.Alternatives select Visit(a));
+            foreach (var a in node.Alternatives)
+            {
+                Alternative alternative;
+                AbstractAlternative abstractAlternative;
 
-            return prod;
+                Visit(a, out alternative, out abstractAlternative);
+
+                prod.Alternatives.Add(alternative);
+                abs.Alternatives.Add(abstractAlternative);
+            }
         }
 
-        public Alternative Visit(PAlternative node)
+        public void Visit(PAlternative node, out Alternative alternative, out AbstractAlternative absAlternative)
         {
-            throw new NotImplementedException();
+            string name = node.Production.Identifier.Text.ToCamelCase();
+            if (node.HasAlternativename)
+                name = node.Alternativename.Name.Text.ToCamelCase() + name;
+            name = "A" + name;
+
+            var elements = new List<Alternative.Element>();
+            var absElements = new List<AbstractAlternative.Element>();
+            var transArgs = new List<Translation>();
+
+            foreach (var e in node.Elements)
+            {
+                Alternative.Element element;
+                AbstractAlternative.Element absElement;
+
+                Visit(e, out element, out absElement);
+
+                Translation trans = new ElementTranslation(element);
+                if (e.IsList)
+                    trans = new ListTranslation(new Translation[] { trans });
+                transArgs.Add(trans);
+            }
+
+            absAlternative = new AbstractAlternative(name, absElements);
+            alternative = new Alternative(elements, new NewTranslation(absAlternative, transArgs));
         }
-        public Alternative.Element Visit(PElement node)
+        public void Visit(PElement node, out Alternative.Element element, out AbstractAlternative.Element absElement)
         {
-            throw new NotImplementedException();
+            string name = node.Elementid.Identifier.Text.ToCamelCase();
+
+            if (node.HasElementname)
+                name = node.Elementname.Name.Text;
+            else if (node.IsList)
+                name += "s";
+
+            var mod = node.Modifier.GetModifier();
+            var id = node.Elementid.Identifier;
+
+            if (id.IsPProduction)
+            {
+                var prod = productions[id.AsPProduction];
+                element = new Alternative.ProductionElement(prod, mod);
+                absElement = new AbstractAlternative.ProductionElement(name.ToCamelCase(), abstractProductions[prod], mod);
+            }
+            else if (id.IsPToken)
+            {
+                element = new Alternative.TokenElement(tokens[id.AsPToken], mod);
+                absElement = new AbstractAlternative.TokenElement(name.ToCamelCase(), tokens[id.AsPToken], mod);
+            }
+            else
+                throw new ArgumentException("Element must must refer to either an ast node or a token.");
         }
     }
 }
