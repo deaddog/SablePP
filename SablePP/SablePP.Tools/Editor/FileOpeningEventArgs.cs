f128d3ff3f8c4959f01066cb1da6bb37fe3b2f44
deaddog <mikkelslsl@gmail.com>
2014-11-07 22:42:05 +0100

    Created an EventArgs type for handling a pre-open event.

---
+++ SablePP/SablePP.Tools/Editor/FileOpeningEventArgs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SablePP.Tools.Editor
+{
+    /// <summary>
+    /// Provides data for the <see cref="EditorForm.FileOpening"/> event.
+    /// </summary>
+    public class FileOpeningEventArgs : EventArgs
+    {
+        private string filepath;
+        private bool allowfile;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileOpeningEventArgs"/> class.
+        /// </summary>
+        /// <param name="filepath">The filepath of the file being opened.</param>
+        /// <param name="initialAllow">An initial value for whether <paramref name="filepath"/> can be opened.</param>
+        public FileOpeningEventArgs(string filepath, bool initialAllow)
+        {
+            this.filepath = filepath;
+            this.allowfile = initialAllow;
+        }
+
+        /// <summary>
+        /// Gets the filepath of the file being opened.
+        /// </summary>
+        public string Filepath
+        {
+            get { return filepath; }
+        }
+        /// <summary>
+        /// Gets or sets a value indicating whether <see cref="Filepath"/> can be opened.
+        /// </summary>
+        public bool AllowFile
+        {
+            get { return allowfile; }
+            set { allowfile = value; }
+        }
+    }
+}
