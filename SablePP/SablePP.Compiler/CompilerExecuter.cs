086c32eea0e10a2cbebcc9b6135196060a7a6f2d
deaddog <mikkelslsl@gmail.com>
2015-05-14 19:53:48 +0200

    Removed the method that performed the grammar rebuild. The parsed (sectioned) grammar is preserved.

--- SablePP/SablePP.Compiler/CompilerExecuter.cs
+++ SablePP/SablePP.Compiler/CompilerExecuter.cs
@@ -34,7 +34,12 @@ namespace SablePP.Compiler
 
         public override void Validate(Start<PGrammar> root, CompilationOptions compilationOptions)
         {
-            Rebuild(root, compilationOptions.ErrorManager);
+            {
+                var packages = sections<APackageSection>(root);
+                for (int i = 1; i < packages.Length; i++)
+                    compilationOptions.ErrorManager.Register(packages[i], "A SablePP grammar cannot contain multiple Package/Namespace sections.");
+            }
+
             if (compilationOptions.ErrorManager.Errors.Count > 0)
                 return;
 
@@ -55,92 +60,6 @@ namespace SablePP.Compiler
                 this.lastGrammar = null;
                 this.lastResult = null;
             }
-        }
-
-        private void Rebuild(Start<PGrammar> root, ErrorManager errorManager)
-        {
-            AGrammar grammar = new AGrammar(null, null, null, null, null, null, null, null);
-
-            { // Packages
-                var packages = sections<APackageSection>(root);
-                for (int i = 1; i < packages.Length; i++)
-                    errorManager.Register(packages[i].Package, "A SablePP grammar cannot contain multiple Package/Namespace sections.");
-
-                if (packages.Length > 0)
-                    grammar.Package = packages[0].Package;
-            }
-
-            { // Helpers
-                var helpers = sections<AHelpersSection>(root);
-                var list = new List<PHelper>();
-                for (int i = 0; i < helpers.Length; i++)
-                    list.AddRange(helpers[i].Helpers.Helpers);
-
-                if (helpers.Length > 0)
-                    grammar.Helpers = new AHelpers(new THelperstoken("Helpers"), list);
-            }
-
-            { // States
-                var states = sections<AStatesSection>(root);
-                var list = new List<PState>();
-                for (int i = 0; i < states.Length; i++)
-                    list.AddRange(states[i].States.States);
-
-                if (states.Length > 0)
-                    grammar.States = new AStates(new TStatestoken("States"), list, new TSemicolon(";"));
-            }
-
-            { // Tokens
-                var tokens = sections<ATokensSection>(root);
-                var list = new List<PToken>();
-                for (int i = 0; i < tokens.Length; i++)
-                    list.AddRange(tokens[i].Tokens.Tokens);
-
-                if (tokens.Length > 0)
-                    grammar.Tokens = new ATokens(new TTokenstoken("Tokens"), list);
-            }
-
-            { // Ignored
-                var ignored = sections<AIgnoreSection>(root);
-                var list = new List<TIdentifier>();
-                for (int i = 0; i < ignored.Length; i++)
-                    list.AddRange(ignored[i].Ignoredtokens.Tokens);
-
-                if (ignored.Length > 0)
-                    grammar.Ignoredtokens = new AIgnoredtokens(new TIgnoredtoken("Ignored"), new TTokenstoken("Tokens"), list, new TSemicolon(";"));
-            }
-
-            { // Productions
-                var productions = sections<AProductionsSection>(root);
-                var list = new List<PProduction>();
-                for (int i = 0; i < productions.Length; i++)
-                    list.AddRange(productions[i].Productions.Productions);
-
-                if (productions.Length > 0)
-                    grammar.Productions = new AProductions(new TProductionstoken("Productions"), list);
-            }
-
-            { // AST Productions
-                var astproductions = sections<AASTSection>(root);
-                var list = new List<PProduction>();
-                for (int i = 0; i < astproductions.Length; i++)
-                    list.AddRange(astproductions[i].Astproductions.Productions);
-
-                if (astproductions.Length > 0)
-                    grammar.Astproductions = new AAstproductions(new TAsttoken("Abstract Syntax Tree"), list);
-            }
-
-            { // Highlight rules
-                var highlight = sections<AHighlightSection>(root);
-                var list = new List<PHighlightrule>();
-                for (int i = 0; i < highlight.Length; i++)
-                    list.AddRange(highlight[i].Highlightrules.Highlightrules);
-
-                if (highlight.Length > 0)
-                    grammar.Highlightrules = new AHighlightrules(new THighlighttoken("Token Syntax Highlight"), list);
-            }
-
-            root.Root = grammar;
         }
 
         private TSection[] sections<TSection>(Start<PGrammar> node) where TSection : PSection
