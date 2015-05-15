42bfc619393fe68b762585d6a9e2e01ee5354877
deaddog <mikkelslsl@gmail.com>
2015-05-15 13:55:12 +0200

    Created specialized methods in the ErrorVisitor for visiting the implicit collections in PGrammar.

--- SablePP/SablePP.Compiler/Validation/ErrorVisitor.cs
+++ SablePP/SablePP.Compiler/Validation/ErrorVisitor.cs
@@ -1,8 +1,9 @@
-﻿using System;
-
+﻿using SablePP.Compiler.Analysis;
+using SablePP.Compiler.Nodes;
 using SablePP.Tools.Error;
 using SablePP.Tools.Nodes;
-using SablePP.Compiler.Analysis;
+using System;
+using System.Collections.Generic;
 
 namespace SablePP.Compiler.Validation
 {
@@ -35,6 +36,44 @@ namespace SablePP.Compiler.Validation
         protected void RegisterMessage(Node node, string errorMessage, params object[] args)
         {
             errorManager.Register(node, ErrorType.Message, errorMessage, args);
+        }
+
+        private void visitEach(IEnumerable<Node> nodes)
+        {
+            foreach (var n in nodes)
+                Visit(n);
+        }
+        public virtual void VisitPackages(IEnumerable<TPackagename> nodes)
+        {
+            visitEach(nodes);
+        }
+        public virtual void VisitHelpers(IEnumerable<PHelper> nodes)
+        {
+            visitEach(nodes);
+        }
+        public virtual void VisitStates(IEnumerable<PState> nodes)
+        {
+            visitEach(nodes);
+        }
+        public virtual void VisitTokens(IEnumerable<PToken> nodes)
+        {
+            visitEach(nodes);
+        }
+        public virtual void VisitIgnoredTokens(IEnumerable<TIdentifier> nodes)
+        {
+            visitEach(nodes);
+        }
+        public virtual void VisitProductions(IEnumerable<PProduction> nodes)
+        {
+            visitEach(nodes);
+        }
+        public virtual void VisitAstProductions(IEnumerable<PProduction> nodes)
+        {
+            visitEach(nodes);
+        }
+        public virtual void VisitHighlightRules(IEnumerable<PHighlightrule> nodes)
+        {
+            visitEach(nodes);
         }
     }
 }
