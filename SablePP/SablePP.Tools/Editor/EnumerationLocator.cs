fbc6a914e640b748c0489180219aa9cb63c73264
deaddog <mikkelslsl@gmail.com>
2015-06-12 10:51:45 +0200

    Created an EnumerationLocator class that provides an abstraction over the FindReferences method.

---
+++ SablePP/SablePP.Tools/Editor/EnumerationLocator.cs
@@ -0,0 +1,19 @@
+﻿using SablePP.Tools.Analysis;
+using SablePP.Tools.Nodes;
+using System.Collections.Generic;
+
+namespace SablePP.Tools.Editor
+{
+    public abstract class EnumerationLocator : IDeclarationLocator
+    {
+        public abstract Token FindDeclaration(Token token);
+
+        public IEnumerable<Token> FindReferences(Token declaration, CodeTextBox.Result result)
+        {
+            foreach (var t in DepthFirstTreeWalker.GetTokens(result.Tree))
+                if (IsReference(declaration, t))
+                    yield return t;
+        }
+        protected abstract bool IsReference(Token declaration, Token token);
+    }
+}
