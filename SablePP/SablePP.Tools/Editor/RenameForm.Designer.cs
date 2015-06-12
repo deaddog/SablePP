558568fad7462555b06fbf66e1955efeb7cca119
deaddog <mikkelslsl@gmail.com>
2015-06-12 22:36:35 +0200

    Generalised the RenameForm (removed all SablePP specific code).

--- SablePP/SablePP.Tools/Editor/RenameForm.Designer.cs
+++ SablePP/SablePP.Tools/Editor/RenameForm.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.cancelButton = new System.Windows.Forms.Button();
             this.renameButton = new System.Windows.Forms.Button();
@@ -40,34 +39,27 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.Size = new System.Drawing.Size(185, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Renaming the {1} \"{0}\" in your grammar.";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Specify a new name below:";
+            this.label1.Text = "Renaming \"{0}\" and all its references.\r\nSpecify a new name below:";
             // 
             // textBox1
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 43);
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(311, 20);
+            this.textBox1.Size = new System.Drawing.Size(311, 22);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // cancelButton
             // 
-            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(248, 69);
+            this.cancelButton.Location = new System.Drawing.Point(248, 66);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 3;
@@ -76,8 +68,8 @@
             // 
             // renameButton
             // 
-            this.renameButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.renameButton.Location = new System.Drawing.Point(167, 69);
+            this.renameButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.renameButton.Location = new System.Drawing.Point(167, 66);
             this.renameButton.Name = "renameButton";
             this.renameButton.Size = new System.Drawing.Size(75, 23);
             this.renameButton.TabIndex = 4;
@@ -89,19 +81,18 @@
             // 
             this.AcceptButton = this.renameButton;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(335, 104);
+            this.ClientSize = new System.Drawing.Size(335, 101);
             this.Controls.Add(this.renameButton);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
-            this.MaximumSize = new System.Drawing.Size(800, 138);
-            this.MinimumSize = new System.Drawing.Size(300, 138);
+            this.MinimumSize = new System.Drawing.Size(300, 100);
             this.Name = "RenameForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Renaming {0}";
+            this.Text = "Renaming \"{0}\"";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,7 +101,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button renameButton;
--- SablePP/SablePP.Tools/Editor/RenameForm.cs
+++ SablePP/SablePP.Tools/Editor/RenameForm.cs
@@ -1,4 +1,4 @@
-﻿using SablePP.Compiler.Nodes;
+﻿using SablePP.Tools.Nodes;
 using System;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
@@ -7,59 +7,38 @@ namespace SablePP.Tools.Editor
 {
     public partial class RenameForm : Form
     {
-        private string type;
-        private string name;
+        private IDeclarationRenamer renamer;
+        private Token token;
 
-        private readonly Regex identifierRegex = new Regex("^[a-z][a-z0-9]*(_[a-z][a-z0-9]*)*$");
-
-        public static string GetDeclarationType(DeclarationIdentifier identifier)
-        {
-            if (identifier.IsPAlternative)
-                return "alternative";
-            else if (identifier.IsPElement)
-                return "production element";
-            else if (identifier.IsPHelper)
-                return "helper";
-            else if (identifier.IsPProduction)
-            {
-                var ast = identifier.AsPProduction.GetFirstParent<AASTSection>();
-                if (ast == null)
-                    return "production";
-                else
-                    return "ast production";
-            }
-            else if (identifier.IsPToken)
-                return "token";
-            else if (identifier.IsState)
-                return "state";
-            else
-                return null;
-        }
-
-        public RenameForm()
+        private RenameForm()
         {
             InitializeComponent();
         }
 
-        public RenameForm(DeclarationIdentifier identifier)
+        public RenameForm(IDeclarationRenamer renamer, Token token)
             : this()
         {
-            this.type = GetDeclarationType(identifier);
-            this.name = identifier.Text;
+            if (renamer == null)
+                throw new ArgumentNullException("renamer");
+            if (token == null)
+                throw new ArgumentNullException("token");
 
-            this.Text = string.Format(this.Text, name);
-            this.label1.Text = string.Format(this.label1.Text, name, type);
-            this.textBox1.Text = name;
+            this.renamer = renamer;
+            this.token = token;
+
+            this.Text = string.Format(this.Text, token.Text);
+            this.label1.Text = string.Format(this.label1.Text, token.Text);
+            this.textBox1.Text = token.Text;
         }
 
-        public string IdentifierName
+        public string DeclarationName
         {
             get { return textBox1.Text; }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            renameButton.Enabled = identifierRegex.IsMatch(textBox1.Text);
+            renameButton.Enabled = renamer.IsNameValid(token, textBox1.Text);
         }
 
         private void renameButton_Click(object sender, EventArgs e)
