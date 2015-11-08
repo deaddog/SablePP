27f112dc45d2f650574de247640783a7ad062c53
deaddog <mikkelslsl@gmail.com>
2015-11-08 01:16:55 +0100

    Implemented the normalized edit distance in the EditDistance struct.

--- SablePP/SablePP.Tools/EditDistance.cs
+++ SablePP/SablePP.Tools/EditDistance.cs
@@ -15,8 +15,7 @@ namespace SablePP.Tools
         /// </summary>
         /// <param name="origin">The original string in the transformation.</param>
         /// <param name="target">The target string in the transformation.</param>
-        /// <param name="distance">The edit distance between the two strings.</param>
-        public EditDistance(string origin, string target, double distance)
+        public EditDistance(string origin, string target)
         {
             if (origin == null)
                 throw new ArgumentNullException(nameof(origin));
@@ -26,7 +25,25 @@ namespace SablePP.Tools
 
             this.origin = origin;
             this.target = target;
-            this.distance = distance;
+            this.distance = editDistanceNormalized(origin, target);
+        }
+
+        private static double editDistanceNormalized(string s, string t)
+        {
+            double I = s.Length;
+            double J = t.Length;
+
+            double[,] dist = new double[s.Length + 1, t.Length + 1];
+            for (int i = 0; i <= s.Length; i++) dist[i, 0] = i / I;
+            for (int j = 1; j <= t.Length; j++) dist[0, j] = j / J;
+            for (int i = 1; i <= s.Length; i++)
+                for (int j = 1; j <= t.Length; j++)
+                    if (s[i - 1] == t[j - 1])
+                        dist[i, j] = Math.Min(dist[i - 1, j - 1], Math.Min(dist[i - 1, j] + 1 / I, dist[i, j - 1] + 1 / J));
+                    else
+                        dist[i, j] = Math.Min(dist[i - 1, j - 1] + 0.5 * (1 / I + 1 / J), Math.Min(dist[i - 1, j] + 1 / I, dist[i, j - 1] + 1 / J));
+
+            return dist[s.Length, t.Length];
         }
 
         /// <summary>
