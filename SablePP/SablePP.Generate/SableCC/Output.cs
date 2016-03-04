5e2a407a788a1d4c9f6efd5e28d949a0b7bb3129
deaddog <mikkelslsl@gmail.com>
2016-03-04 09:41:52 +0100

    Created a new class for representing the output of the SableCC process.

---
+++ SablePP/SablePP.Generate/SableCC/Output.cs
@@ -0,0 +1,17 @@
+﻿namespace SablePP.Generate.SableCC
+{
+    internal class Output
+    {
+        private int exitCode;
+        private string errorString;
+
+        public Output(int exitCode, string errorString)
+        {
+            this.exitCode = exitCode;
+            this.errorString = errorString;
+        }
+
+        public int ExitCode => exitCode;
+        public string ErrorString => errorString;
+    }
+}
