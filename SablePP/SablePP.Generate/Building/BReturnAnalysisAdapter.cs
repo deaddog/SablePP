1248d1a34a9eae2720b3321532adce5f814c85ea
deaddog <mikkelslsl@gmail.com>
2016-03-04 08:33:53 +0100

    Implement the new analysis code-gen scheme for the return adapters.

--- SablePP/SablePP.Generate/Building/BReturnAnalysisAdapter.cs
+++ SablePP/SablePP.Generate/Building/BReturnAnalysisAdapter.cs
@@ -24,34 +24,76 @@ namespace SablePP.Generate.Building
                 types.Add("T" + i);
             types.Add(returnTypeParameter);
 
+            bool first = true;
+
             foreach (var p in node.AbstractProductions)
+            {
+                if (!first)
+                    returnAnalysisAdapter.EmitNewline();
+
+                emitReturnAnalysisAdapterProduction(returnAnalysisAdapter, p);
                 foreach (var a in p.Alternatives)
-                    emitReturnAnalysisAdapterCase(returnAnalysisAdapter, a.Name, arguments);
+                    emitReturnAnalysisAdapterAlternative(returnAnalysisAdapter, a);
+
+                first = false;
+            }
+
+            returnAnalysisAdapter.EmitNewline();
 
             foreach (var t in node.Tokens)
-                emitReturnAnalysisAdapterCase(returnAnalysisAdapter, t.Name, arguments);
+                emitReturnAnalysisAdapterToken(returnAnalysisAdapter, t);
         }
 
-        private void emitReturnAnalysisAdapterCase(ClassElement returnAnalysisAdapter, string name, int arguments)
+        private void emitReturnAnalysisAdapterToken(ClassElement adapterClass, Token node)
         {
-            var types = returnAnalysisAdapter.TypeParameters;
+            var types = adapterClass.TypeParameters;
 
             MethodElement method;
-            returnAnalysisAdapter.Add(method = new MethodElement($"public {types[arguments]} Visit({name} node)"));
-            for (int i = 1; i <= arguments; i++)
-                method.Parameters.Add("{0} arg{1}", returnAnalysisAdapter.TypeParameters[i - 1], i);
+            method = createReturnMethod(adapterClass, $"public {types[types.Count - 1]} Visit({node.Name} node)");
+            emitReturnStatement(method, $"Handle{node.Name}(node");
 
-            method.Body.Emit($"return Handle{name}(node");
-            for (int i = 1; i <= arguments; i++)
-                method.Body.Emit(", arg{0}", i);
-            method.Body.EmitLine(");");
+            method = createReturnMethod(adapterClass, $"protected virtual {types[types.Count - 1]} Handle{node.Name}({node.Name} node)");
+            emitReturnStatement(method, $"HandleDefault(node");
+        }
+        private void emitReturnAnalysisAdapterAlternative(ClassElement adapterClass, AbstractAlternative node)
+        {
+            var types = adapterClass.TypeParameters;
 
-            returnAnalysisAdapter.Add(method = new MethodElement($"public virtual {types[arguments]} Handle{name}({name} node)"));
-            for (int i = 1; i <= arguments; i++)
-                method.Parameters.Add($"{types[i - 1]} arg{i}");
+            MethodElement method;
+            method = createReturnMethod(adapterClass, $"private {types[types.Count - 1]} dispatch({node.Name} node)");
+            emitReturnStatement(method, $"Handle{node.Name}(node");
 
-            method.Body.Emit("return HandleDefault(node");
-            for (int i = 1; i <= arguments; i++)
+            method = createReturnMethod(adapterClass, $"protected virtual {types[types.Count - 1]} Handle{node.Name}({node.Name} node)");
+            emitReturnStatement(method, $"HandleDefault(node");
+        }
+        private void emitReturnAnalysisAdapterProduction(ClassElement adapterClass, AbstractProduction node)
+        {
+            var types = adapterClass.TypeParameters;
+
+            MethodElement method;
+            method = createReturnMethod(adapterClass, $"public {types[types.Count - 1]} Visit({node.Name} node)");
+            emitReturnStatement(method, $"Handle{node.Name}(node");
+
+            method = createReturnMethod(adapterClass, $"protected virtual {types[types.Count - 1]} Handle{node.Name}({node.Name} node)");
+            emitReturnStatement(method, "dispatch((dynamic)node");
+        }
+
+        private MethodElement createReturnMethod(ClassElement adapterClass, string methodsignature, bool hasBody = true)
+        {
+            var types = adapterClass.TypeParameters;
+
+            var method = new MethodElement(methodsignature, hasBody);
+            adapterClass.Add(method);
+
+            for (int i = 0; i < types.Count - 1; i++)
+                method.Parameters.Add($"{types[i]} arg{i + 1}");
+
+            return method;
+        }
+        private void emitReturnStatement(MethodElement method, string calledMethod)
+        {
+            method.Body.Emit("return " + calledMethod);
+            for (int i = 1; i < method.Parameters.Count; i++)
                 method.Body.Emit($", arg{i}");
             method.Body.EmitLine(");");
         }
